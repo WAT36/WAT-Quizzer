e1312e2f5fa767f3ae917df6be22d56cd36f566b
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2022-06-15 00:13:45 +0900

    dotenvを追加して　設定ファイルから値読み込みテスト（設定ファイルは非追跡）

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,16 @@
-var express = require("express");
+const express = require("express");
+const dotenv = require("dotenv");
+const path = require('path');
+
+const env = dotenv.config({path: path.join(__dirname, "../config/.env")})
 
 var app = express();
 app.get("/", (req, res) => {
     res.status(200).send("Express!!");
+});
+
+app.get("/namelist", (req, res) => {
+    res.status(200).send(process.env.TEST);
 });
 
 // ポート7000番でlistenする
