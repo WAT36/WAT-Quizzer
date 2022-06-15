173bc4e2cf679a9d33f93ac17739bb5c770d8bf9
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2022-06-15 23:26:06 +0900

    DB接続関数　清書

--- backend/common/Database.js
+++ backend/common/Database.js
@@ -4,27 +4,16 @@ const mysql = require('mysql2');
 
 const env = dotenv.config({path: path.join(__dirname, "../../config/.env")})
 
+// DB接続子を返す
+const getConnection = function(){
+    // DB接続情報
+    return mysql.createConnection({ 
+        host : process.env.DB_HOSTNAME, 
+        user : process.env.DB_USERNAME, 
+        password : process.env.DB_PASSWORD, 
+        port : process.env.DB_PORT, 
+        database: process.env.DB_DBNAME
+    });
+}
+exports.getConnection = getConnection;
 
-// DB接続情報
-let connection = mysql.createConnection({ 
-	host : process.env.DB_HOSTNAME, 
-	user : process.env.DB_USERNAME, 
-	password : process.env.DB_PASSWORD, 
-	port : process.env.DB_PORT, 
-	database: process.env.DB_DBNAME
-});
-
-// DB接続
-connection.connect();
-
-// 試しに何か投げる
-connection.query('SELECT * FROM quiz_file;', (err, rows) => {
-    if (err){
-        console.error(err);
-    };
-
-    console.log("Rows:",rows);
-})
-
-// 接続終了
-connection.end();
