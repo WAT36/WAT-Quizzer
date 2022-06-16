a5a4ff61585c23d77db333bafca0058a69dbcf69
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2022-06-16 15:28:35 +0900

    データベースのクエリ実行関数を分け　非同期処理むずい・・

--- backend/common/Database.js
+++ backend/common/Database.js
@@ -5,15 +5,45 @@ const mysql = require('mysql2');
 const env = dotenv.config({path: path.join(__dirname, "../../config/.env")})
 
 // DB接続子を返す
-const getConnection = function(){
-    // DB接続情報
-    return mysql.createConnection({ 
+const pool = mysql.createPool({ 
         host : process.env.DB_HOSTNAME, 
         user : process.env.DB_USERNAME, 
         password : process.env.DB_PASSWORD, 
         port : process.env.DB_PORT, 
         database: process.env.DB_DBNAME
-    });
-}
-exports.getConnection = getConnection;
+});
 
+// SQLを実行する
+const execQuery = async (query) => {
+    try{
+        // DB接続
+        const connection = await new Promise((resolve,reject) => {
+            pool.getConnection((error, connection) => {
+                if(error){
+                    reject(error);
+                }
+                resolve(connection);
+            })
+        })
+
+        // クエリ実行
+        const result = await new Promise((resolve,reject) => {
+            connection.query(query, (error, result) => {
+                if(error){
+                    reject(error);
+                }
+                resolve(result);
+            })    
+        })
+
+        // 接続終了
+        await connection.release();
+        return result;
+    } catch(error) {
+        throw error;
+    }
+}
+
+module.exports = {
+    execQuery,
+};
