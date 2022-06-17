4fdc5c26726387b075da9e65b0016314052d25cc
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2022-06-17 15:23:44 +0900

    ファイルリスト　APIから取得して画面に表示　に成功！！

--- frontend/src/pages/SelectQuizPage.jsx
+++ frontend/src/pages/SelectQuizPage.jsx
@@ -5,35 +5,48 @@ const buttonStyle = {
     'margin'     :  '10px',
 }
 class SelectQuizPage extends React.Component{
+    componentDidMount(){
+        fetch('http://localhost:4000/namelist')
+            .then(response => response.json())
+            .then(data => {
+                let filelist = []
+                for(var i=0;i<data.length;i++){
+                    filelist.push(<MenuItem value={data[i].file_num}>{data[i].file_nickname}</MenuItem>)
+                }
+                this.setState({
+                    filelistoption: filelist,
+                })
+            }).catch(error => {
+                console.error("componentDidMount:",error)
+            })
+    }
 
     constructor(props){
         super(props);
-        this.fileSelect = this.fileSelect.bind(this);
         this.rangeSlider = this.rangeSlider.bind(this);
         this.state = {
             value: [20,37]
         }
     }
 
-    fileSelect = () => {
-        return <MenuItem value={0}>追加テスト</MenuItem>
-    }
-
     rangeSlider = () => {
-        // const [value, setValue] = React.useState(20);
     
         const handleChange = (event, newValue) => {
-            // setValue(newValue);
             this.setState({value: newValue})
         };
     
         return (
+            <>
+                <Typography id="range-slider" gutterBottom>
+                正解率(%)指定
+                </Typography>
                 <Slider
                 value={this.state.value}
                 onChange={handleChange}
                 valueLabelDisplay="auto"
                 aria-labelledby="range-slider"
                 />
+            </>
         );
     } 
 
@@ -50,8 +63,8 @@ class SelectQuizPage extends React.Component{
                             // value={age}
                             // onChange={handleChange}
                         >
-                            <MenuItem value={-1}>選択肢テスト</MenuItem>
-                            {this.fileSelect()}
+                            <MenuItem value={-1}>選択なし</MenuItem>
+                            {this.state.filelistoption}
                         </Select>
                     </FormControl>
 
@@ -72,16 +85,8 @@ class SelectQuizPage extends React.Component{
                     </FormControl>
 
                     <FormControl>
-                        <Typography id="range-slider" gutterBottom>
-                            正解率(%)指定
-                        </Typography>
-                        {/* <Slider
-                            value={this.defaultAccRate}
-                            onChange={this.handleChange}
-                            valueLabelDisplay="on"
-                            aria-labelledby="range-slider"
-                            // getAriaValueText={valuetext}
-                        /> */}
+
+
                         {this.rangeSlider()}
                     </FormControl>
 
