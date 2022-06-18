dc6fb658c80934bcf7a7f1babf6d6feea72b694a
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2022-06-18 19:30:12 +0900

    フロント->バックへのAPI送信機能を別離

---
+++ frontend/src/common/API.jsx
@@ -0,0 +1,28 @@
+const baseURL = 'http://localhost:4000'
+
+const get = (path,func) => {
+  fetch(baseURL + path)
+  .then(response => response.json())
+  .then(func
+  ).catch(error => {
+      console.error("componentDidMount:",error)
+  });
+}
+
+const post = (path,jsondata,func) => {
+  fetch(baseURL + path,{
+    method: 'POST',
+    body: JSON.stringify(jsondata),
+    headers: {'Content-Type': 'application/json'},
+  })
+  .then(response => response.json())
+  .then(func
+  ).catch(error => {
+      console.error("componentDidMount:",error)
+  });
+}
+
+module.exports = {
+  get,
+  post,
+};
