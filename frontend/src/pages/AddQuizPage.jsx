0a296aa4bfe2657d053bf27779e7a5343fcfe838
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2022-06-22 14:07:09 +0900

    問題追加ページ　デザイン的に直したいとこはあるが機能面では完成

--- frontend/src/pages/AddQuizPage.jsx
+++ frontend/src/pages/AddQuizPage.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Card, CardContent, Container, FormControl, InputLabel, MenuItem, Select, FormGroup, Typography } from "@material-ui/core"
+import { Button, Card, CardContent, Container, FormControl, InputLabel, MenuItem, Select, FormGroup, Typography, TextField } from "@material-ui/core"
 
 import API from "../common/API";
 import QuizzerLayout from "./components/QuizzerLayout";
@@ -7,6 +7,16 @@ import QuizzerLayout from "./components/QuizzerLayout";
 const messageBoxStyle = {
     'margin'        : '10px 0px 20px',
     'borderStyle'  : 'none'
+}
+
+const buttonStyle = {
+    'margin'     :  '10px',
+}
+
+const typoStyles = {
+    title: {
+        fontSize: 14,
+    },
 }
 
 export default class AddQuizPage extends React.Component{
@@ -34,9 +44,47 @@ export default class AddQuizPage extends React.Component{
         super(props);
         this.state = {
             file_num: -1,
+            input_data: "",
             message: '　',
             messageColor: 'initial',
         }
+    }
+
+    selectedFileChange = (e) => {
+        this.setState({
+            file_num: e.target.value,
+        })
+    }
+
+    addQuiz = () => {
+        if(this.state.file_num === -1){
+            this.setState({
+                message: 'エラー:問題ファイルを選択して下さい',
+                messageColor: 'error',
+            })
+            return;
+        }
+
+        API.post("/add",{
+            "file_num": this.state.file_num,
+            "data": this.state.input_data
+        },(data) => {
+            if(data.status === 200){
+                data = data.body
+                this.setState({
+                    message: '　',
+                    messageColor: 'initial',
+                    addLog: data,
+                })
+                //入力データをクリア
+                document.getElementsByClassName("input-quiz-fields").value = ""
+            }else{
+                this.setState({
+                    message: 'エラー:外部APIとの連携に失敗しました',
+                    messageColor: 'error',
+                })
+            }
+        });
     }
 
     contents = () => {
@@ -60,16 +108,48 @@ export default class AddQuizPage extends React.Component{
                             id="quiz-file-id"
                             defaultValue={-1}
                             // value={age}
-                            // onChange={(e) => this.selectedFileChange(e)}
+                            onChange={(e) => this.selectedFileChange(e)}
                         >
                             <MenuItem value={-1} key={-1}>選択なし</MenuItem>
                             {this.state.filelistoption}
                         </Select>
                     </FormControl>
 
+                    <Typography variant="h6" component="h6" style={messageBoxStyle}>
+                        追加する問題（形式：テスト問題,正解,カテゴリ,画像ファイル名）
+                    </Typography>
+
+                    <TextField
+                        label="追加問題入力"
+                        className="input-quiz-fields"
+                        multiline
+                        placeholder="追加する問題（形式：テスト問題,正解,カテゴリ,画像ファイル名）を何行でも"
+                        minRows={6}
+                        variant="outlined"
+                        onChange={(e) => this.setState({input_data: e.target.value})}
+                    />
 
                 </FormGroup>
 
+                <Button 
+                    style={buttonStyle} 
+                    variant="contained" 
+                    color="primary"
+                    onClick={(e) => this.addQuiz()}
+                    >
+                    送信
+                </Button>
+
+                <Card variant="outlined">
+                    <CardContent>
+                        <Typography className={typoStyles.title} color="textSecondary" gutterBottom>
+                            --実行ログ--
+                        </Typography>
+                        <Typography variant="h6" component="h6">
+                            {this.state.addLog}
+                        </Typography>
+                    </CardContent>
+                </Card>
 
             </Container>
         )
