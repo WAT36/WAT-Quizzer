ae38658532ebaf3515664de9f495a751bd5a48e7
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2022-06-24 22:14:25 +0900

    (Jestによるテストにより)DB接続方法を変更した（！）

--- backend/common/Database.js
+++ backend/common/Database.js
@@ -5,26 +5,19 @@ const mysql = require('mysql2');
 const env = dotenv.config({path: path.join(__dirname, "../../config/.env")})
 
 // DB接続子を返す
-const pool = mysql.createPool({ 
-        host : process.env.DB_HOSTNAME, 
-        user : process.env.DB_USERNAME, 
-        password : process.env.DB_PASSWORD, 
-        port : process.env.DB_PORT, 
-        database: process.env.DB_DBNAME
-});
+const dbinfo = { 
+    host : process.env.DB_HOSTNAME, 
+    user : process.env.DB_USERNAME, 
+    password : process.env.DB_PASSWORD, 
+    port : process.env.DB_PORT, 
+    database: process.env.DB_DBNAME
+};
 
 // SQLを実行する
 const execQuery = async (query, value) => {
     try{
         // DB接続
-        const connection = await new Promise((resolve,reject) => {
-            pool.getConnection((error, connection) => {
-                if(error){
-                    reject(error);
-                }
-                resolve(connection);
-            })
-        })
+        const connection = mysql.createConnection(dbinfo);
 
         // クエリ実行
         const result = await new Promise((resolve,reject) => {
@@ -37,7 +30,7 @@ const execQuery = async (query, value) => {
         })
 
         // 接続終了
-        await connection.release();
+        await connection.end();
         return result;
     } catch(error) {
         throw error;
