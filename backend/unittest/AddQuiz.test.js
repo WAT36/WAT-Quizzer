8cacc69544cb39a90c5152767d7a35e2d93ef788
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2022-06-24 22:15:08 +0900

    試しに問題追加を行うテストファイル作成（エラーづくしでキツかった）

--- backend/unittest/AddQuiz.test.js
+++ backend/unittest/AddQuiz.test.js
@@ -1,18 +1,20 @@
+require('iconv-lite').encodingExists('foo');
+
 const testCommon = require('./TestCommon');
 const addQuiz = require('../services/QuizService').addQuiz;
 
+// 何かひとつ問題を追加するテスト
+test('Add 1 Quiz.',async () => {
 
-// 何かひとつ問題を追加する
-test('Add 1 Quiz.',() => {
     // まず全消し
-    testCommon.deleteAllQuizOfFile(0);
+    let result = await testCommon.deleteAllQuizOfFile(0);
 
     // 問題追加
-    addQuiz(0,"addQuizテスト問題,addQuizテスト答え,addQuizテストカテゴリ,addQuizテスト画像");
+    result = await addQuiz(0,"addQuizテスト問題,addQuizテスト答え,addQuizテストカテゴリ,addQuizテスト画像");
 
     // 問題取得
-    let data = testCommon.getAllQuizOfFileService(0);
-    console.log("datatest:",data)
+    let data = await testCommon.getAllQuizOfFileService(0);
+
     // 確認
     expect(data.length).toBe(1);
     expect(data[0].quiz_sentense).toBe('addQuizテスト問題');
