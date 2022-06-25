55981a0f363158295f23570fd1e7b3647f76dd8b
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2022-06-25 23:14:39 +0900

    npm用google-chartインストール、サンプルチャート表示

--- frontend/src/pages/AccuracyRateGraphPage.jsx
+++ frontend/src/pages/AccuracyRateGraphPage.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { Button, Card, CardContent, Container, FormControl, InputLabel, MenuItem, Select, FormGroup, Typography, TextField, Input } from "@material-ui/core"
+import { Button, Card, CardContent, Container, FormControl, InputLabel, MenuItem, Select, FormGroup, Typography } from "@material-ui/core"
+import { Chart } from "react-google-charts";
 
 import API from "../common/API";
 import QuizzerLayout from "./components/QuizzerLayout";
@@ -37,11 +38,53 @@ export default class AccuracyRateGraphPage extends React.Component{
 
     constructor(props){
         super(props);
+        this.displayChart = this.displayChart.bind(this);
         this.state = {
             file_num: -1,
             message: '　',
             messageColor: 'initial',
         }
+    }
+
+    displayChart = () => {
+
+        let data = [
+            [
+                "Element",
+                "Density",
+                { role: "style" },
+                {
+                    sourceColumn: 0,
+                    role: "annotation",
+                    type: "string",
+                    calc: "stringify",
+                },
+            ],
+            ["Copper", 8.94, "#b87333", null],
+            ["Silver", 10.49, "silver", null],
+            ["Gold", 19.3, "gold", null],
+            ["Platinum", 21.45, "color: #e5e4e2", null],
+        ];
+
+        const options = {
+            title: "Density of Precious Metals, in g/cm^3",
+            width: 600,
+            height: 400,
+            bar: { groupWidth: "95%" },
+            legend: { position: "none" },
+        };
+
+        return (
+            <>
+                <Chart 
+                    chartType="BarChart"
+                    width="100%"
+                    height="400px"
+                    data={data}
+                    options={options}
+                />
+            </>
+        )
     }
 
     contents = () => {
@@ -82,6 +125,8 @@ export default class AccuracyRateGraphPage extends React.Component{
                     >
                     正解率表示
                 </Button>
+
+                {this.displayChart()}
 
             </Container>
         )
