d469dc4f23f4955b9e7b995e6b5a29b35c776b76
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2022-06-29 17:17:18 +0900

    問題追加後に問題を取得するテスト

--- backend/unittest/AddQuiz.test.js
+++ backend/unittest/AddQuiz.test.js
@@ -22,3 +22,23 @@ test('Add 1 Quiz.',async () => {
     expect(data[0].category).toBe('addQuizテストカテゴリ');
     expect(data[0].img_file).toBe('addQuizテスト画像');
 });
+
+// 問題を取得するテスト
+test('Get 1 Quiz.',async () => {
+
+    // まず全消し
+    let result = await testCommon.deleteAllQuizOfFile(0);
+
+    // 問題追加
+    result = await QuizService.addQuiz(0,"getQuizテスト問題,getQuizテスト答え,getQuizテストカテゴリ,getQuizテスト画像");
+
+    // 問題取得
+    let data = await QuizService.getQuiz(0,1);
+
+    // 確認
+    expect(data.length).toBe(1);
+    expect(data[0].quiz_sentense).toBe('getQuizテスト問題');
+    expect(data[0].answer).toBe('getQuizテスト答え');
+    expect(data[0].category).toBe('getQuizテストカテゴリ');
+    expect(data[0].img_file).toBe('getQuizテスト画像');
+});
