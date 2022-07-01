6535fb5b33fffbb462d6db114bef91be8b3ce27e
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2022-07-01 10:46:05 +0900

    最低正解率取得テスト

--- backend/unittest/QuizService.test.js
+++ backend/unittest/QuizService.test.js
@@ -350,7 +350,6 @@ test('Random Get 1 of 5 Quiz by rate and category.',async () => {
     expect(data[0].deleted).toBe(0);
 });
 
-
 // ランダム取得 正解率×カテゴリ×チェック
 test('Random Get 1 of 5 Quiz by rate and category and checked.',async () => {
 
@@ -392,5 +391,38 @@ test('Random Get 1 of 5 Quiz by rate and category and checked.',async () => {
     expect(data[0].category).toBe('カテゴリ0:カテゴリ1');
     expect(data[0].img_file).toBe('addQuizテスト画像1');
     expect(data[0].checked).toBe(1);
+    expect(data[0].deleted).toBe(0);
+});
+
+// 最低正解率取得
+test('Worst Rate of 5 Quiz.',async () => {
+
+    // まず全消し
+    let result = await testCommon.deleteAllQuizOfFile(0);
+
+    // 問題追加
+    result = await QuizService.addQuiz(0,add_fivequizs);
+
+    // 正解率
+    result = await testCommon.updateAnswerNumOfQuiz(200,800,0,1);//20%
+    result = await testCommon.updateAnswerNumOfQuiz(4,6,0,2);//40%
+    result = await testCommon.updateAnswerNumOfQuiz(6,4,0,3);//60%
+    result = await testCommon.updateAnswerNumOfQuiz(8,2,0,4);//80%
+    result = await testCommon.updateAnswerNumOfQuiz(1,0,0,5);//100%
+    
+    // 最低正解率問題取得
+    let data = await QuizService.getWorstRateQuiz(0,null,false);
+
+    // 確認
+    expect(data.length).toBe(1);
+    expect(data[0].file_num).toBe(0);
+    expect(data[0].quiz_num).toBe(1);
+    expect(data[0].quiz_sentense).toBe('addQuizテスト問題1');
+    expect(data[0].answer).toBe('addQuizテスト答え1');
+    expect(data[0].clear_count).toBe(200);
+    expect(data[0].fail_count).toBe(800);
+    expect(data[0].category).toBe('addQuizテストカテゴリ1');
+    expect(data[0].img_file).toBe('addQuizテスト画像1');
+    expect(data[0].checked).toBe(0);
     expect(data[0].deleted).toBe(0);
 });
