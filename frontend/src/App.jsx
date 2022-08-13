0661734923b715820e2b79e2cf18fbe5bc90d9f0
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2022-08-13 22:10:05 +0900

    トップページの階層変更

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -34,23 +34,24 @@ class App extends React.Component {
           <Route path="/imageupload" element={<ImageUploadPage />} />
 
           <Route path="/" element={
-            <div className="App">
-              <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                  Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                  className="App-link"
-                  href="https://reactjs.org"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  Learn React
-                </a>
-                <a href="/top">My Top</a>
-              </header>
-            </div>
+            // <div className="App">
+            //   <header className="App-header">
+            //     <img src={logo} className="App-logo" alt="logo" />
+            //     <p>
+            //       Edit <code>src/App.js</code> and save to reload.
+            //     </p>
+            //     <a
+            //       className="App-link"
+            //       href="https://reactjs.org"
+            //       target="_blank"
+            //       rel="noopener noreferrer"
+            //     >
+            //       Learn React
+            //     </a>
+            //     <a href="/top">My Top</a>
+            //   </header>
+            // </div>
+            <TopPage />
           } />
         </Routes>
       </BrowserRouter>
