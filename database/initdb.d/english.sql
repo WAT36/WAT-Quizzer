12c379f1dc52434bab0131b9762613d29b632d01
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2022-09-15 23:18:55 +0900

    auto_incrementと作る順番変更

--- database/initdb.d/english.sql
+++ database/initdb.d/english.sql
@@ -3,41 +3,41 @@ USE english_db;
 
 CREATE TABLE IF NOT EXISTS word
 (
-  id INT NOT NULL,
+  id INT NOT NULL AUTO_INCREMENT,
   name VARCHAR(256) NOT NULL,
   pronounce VARCHAR(256) NOT NULL,
-  PRIMARY KEY(id) AUTO_INCREMENT
+  PRIMARY KEY(id) 
+) DEFAULT CHARACTER
+  SET=utf8;
+
+CREATE TABLE IF NOT EXISTS partsofspeech
+(
+  id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(256) NOT NULL,
+  PRIMARY KEY(id)
 ) DEFAULT CHARACTER
   SET=utf8;
 
 CREATE TABLE IF NOT EXISTS mean
 (
-  id INT NOT NULL,
+  id INT NOT NULL AUTO_INCREMENT,
   word_id INT NOT NULL,
   wordmean_id INT NOT NULL,
   partsofspeech_id INT NOT NULL,
   meaning VARCHAR(256) NOT NULL,
-  PRIMARY KEY(id) AUTO_INCREMENT,
+  PRIMARY KEY(id),
   UNIQUE wordmean_index (word_id,wordmean_id),
   FOREIGN KEY word_id_foreign_key (word_id) REFERENCES word(id),
   FOREIGN KEY partsofspeech_id_foreign_key (partsofspeech_id) REFERENCES partsofspeech(id)
 ) DEFAULT CHARACTER
   SET=utf8;
 
-CREATE TABLE IF NOT EXISTS partsofspeech
-(
-  id INT NOT NULL,
-  name VARCHAR(256) NOT NULL,
-  PRIMARY KEY(id) AUTO_INCREMENT
-) DEFAULT CHARACTER
-  SET=utf8;
-
 CREATE TABLE IF NOT EXISTS example
 (
-  id INT NOT NULL,
+  id INT NOT NULL AUTO_INCREMENT,
   en_example_sentense VARCHAR(256) NOT NULL,
   ja_example_sentense VARCHAR(256) NOT NULL,
-  PRIMARY KEY(id) AUTO_INCREMENT
+  PRIMARY KEY(id)
 ) DEFAULT CHARACTER
   SET=utf8;
 
@@ -53,22 +53,22 @@ CREATE TABLE IF NOT EXISTS mean_example
 
 CREATE TABLE IF NOT EXISTS idiom
 (
-  id INT NOT NULL,
+  id INT NOT NULL AUTO_INCREMENT,
   word_id INT NOT NULL,
   name VARCHAR(256) NOT NULL,
-  PRIMARY KEY(id) AUTO_INCREMENT,
+  PRIMARY KEY(id),
   FOREIGN KEY word_id_foreign_key (word_id) REFERENCES word(id)
 ) DEFAULT CHARACTER
   SET=utf8;
 
 CREATE TABLE IF NOT EXISTS idiom_mean
 (
-  id INT NOT NULL,
+  id INT NOT NULL AUTO_INCREMENT,
   idiom_id INT NOT NULL,
   idiommean_id INT NOT NULL,
   partsofspeech_id INT NOT NULL,
   meaning VARCHAR(256) NOT NULL,
-  PRIMARY KEY(id) AUTO_INCREMENT,
+  PRIMARY KEY(id),
   UNIQUE idiommean_index (idiom_id,idiommean_id),
   FOREIGN KEY idiom_id_foreign_key (idiom_id) REFERENCES idiom(id),
   FOREIGN KEY partsofspeech_id_foreign_key (partsofspeech_id) REFERENCES partsofspeech(id)
