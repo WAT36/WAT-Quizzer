277d43c6a35f46c65d6d3aa5ad5e77f052814cc4
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2022-09-26 22:55:57 +0900

    とりあえず　英単語追加ページを追加

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react"
 import './App.css';
 
-import {BrowserRouter, Routes, Route} from "react-router-dom"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 import TopPage from './pages/TopPage';
 import SelectQuizPage from './pages/SelectQuizPage';
 import AddQuizPage from "./pages/AddQuizPage";
@@ -12,9 +12,10 @@ import AccuracyRateGraphPage from "./pages/AccuracyRateGraphPage";
 import ImageUploadPage from "./pages/ImageUploadPage";
 
 import EnglishBotTopPage from "./pages/english_bot/EnglishBotTopPage";
+import EnglishBotAddWordPage from "./pages/english_bot/EnglishBotAddWordPage";
 
 class App extends React.Component {
-  render(){
+  render() {
     return (
       <BrowserRouter>
         <Routes>
@@ -35,6 +36,8 @@ class App extends React.Component {
           <Route path="/imageupload" element={<ImageUploadPage />} />
 
           <Route path="/english/top" element={<EnglishBotTopPage />} />
+
+          <Route path="/english/add" element={<EnglishBotAddWordPage />} />
 
           <Route path="/" element={
             <TopPage />
---
+++ frontend/src/pages/english_bot/EnglishBotAddWordPage.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+import EnglishBotLayout from "./components/EnglishBotLayout";
+
+
+class EnglishBotAddWordPage extends React.Component {
+    render() {
+        return (
+            <>
+                <EnglishBotLayout
+                    contents={"単語追加ページ"}
+                />
+            </>
+        )
+    }
+
+}
+
+export default EnglishBotAddWordPage;
