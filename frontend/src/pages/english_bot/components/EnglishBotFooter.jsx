0f1e0ba1d71957e3da8044033d5f2b9861baa01b
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2022-10-30 16:51:57 +0900

    フッター関数コンポーネント 化

--- frontend/src/pages/english_bot/components/EnglishBotFooter.jsx
+++ frontend/src/pages/english_bot/components/EnglishBotFooter.jsx
@@ -23,24 +23,22 @@ const rightStyle = {
     'lineHeight': '30px',
 }
 
-export default class EnglishBotFooter extends React.Component{
-    render(){
-        return (
-            <footer style={footerStyle}>
-                <span className="left" style={leftStyle}>
-                    <Button size="small" color="inherit" href="/top">トップ</Button>
-                    {/* <Link to="/selectquiz">問題出題</Link>
-                    <Link to="/addquiz">問題追加</Link>
-                    <Link to="/editquiz">問題編集</Link>
-                    <Link to="/searchquiz">問題検索</Link>
-                    <Link to="/deletequiz">問題削除</Link>
-                    <Link to="/accuracyrategraph">カテゴリ別正解率表示</Link>
-                    <Link to="/imageupload">画像アップロード</Link> */}
-                </span>
-                <span className="right" style={rightStyle}>
+export default function EnglishBotFooter() {
+    return (
+        <footer style={footerStyle}>
+            <span className="left" style={leftStyle}>
+                <Button size="small" color="inherit" href="/top">トップ</Button>
+                {/* <Link to="/selectquiz">問題出題</Link>
+                <Link to="/addquiz">問題追加</Link>
+                <Link to="/editquiz">問題編集</Link>
+                <Link to="/searchquiz">問題検索</Link>
+                <Link to="/deletequiz">問題削除</Link>
+                <Link to="/accuracyrategraph">カテゴリ別正解率表示</Link>
+                <Link to="/imageupload">画像アップロード</Link> */}
+            </span>
+            <span className="right" style={rightStyle}>
                 ©️ Tatsuroh Wakasugi
-                </span>
-            </footer>
-        )
-    }
+            </span>
+        </footer>
+    )
 }
