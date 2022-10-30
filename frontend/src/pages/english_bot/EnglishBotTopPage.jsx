672ee0306cdd6346b2ce1a7c4ce736b974c93d25
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2022-10-30 16:45:08 +0900

    トップページ関数コンポーネント化

--- frontend/src/pages/english_bot/EnglishBotTopPage.jsx
+++ frontend/src/pages/english_bot/EnglishBotTopPage.jsx
@@ -3,17 +3,13 @@ import React from "react";
 import EnglishBotLayout from "./components/EnglishBotLayout";
 
 
-class EnglishBotTopPage extends React.Component {
-    render() {
-        return (
-            <>
-                <EnglishBotLayout
-                    contents={"準備中・・・\nここはダッシュボードか何かにする"}
-                />
-            </>
-        )
-    }
+export default function EnglishBotTopPage() {
+    return (
+        <>
+            <EnglishBotLayout
+                contents={"準備中・・・\nここはダッシュボードか何かにする"}
+            />
+        </>
+    )
 
 }
-
-export default EnglishBotTopPage;
