371ac3f3fde4411f05ab5a82b043b769a3cdad2a
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2022-12-01 00:30:06 +0900

    postできない問題は治った　引数違い

--- frontend/src/pages/english_bot/EnglishBotDictinoaryPage.jsx
+++ frontend/src/pages/english_bot/EnglishBotDictinoaryPage.jsx
@@ -1,10 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Container from '@mui/material/Container';
+import API from "../../common/API";
 
 import EnglishBotLayout from "./components/EnglishBotLayout";
 
 
 export default function EnglishBotDictionaryPage() {
+
+    const [tableData, setTableData] = useState([])
+    const [message, setMessage] = useState({
+        message: '',
+        messageColor: 'initial',
+    })
+
+    useEffect(() => {
+        comingDisplay();
+    }, [])
+
+    const comingDisplay = () => {
+        API.post("/english/word/search", {
+            "wordName": ""
+        }, (data) => {
+            if (data.status === 200) {
+                setTableData(data)
+            } else {
+                setMessage({
+                    message: 'エラー:外部APIとの連携に失敗しました',
+                    messageColor: 'error',
+                })
+            }
+        })
+    }
 
     const contents = () => {
         return (
