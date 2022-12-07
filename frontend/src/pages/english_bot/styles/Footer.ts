9dae4f44e5745b087ab39b9f723b0506a42a9d64
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2022-12-07 23:10:16 +0900

    Footerも

--- frontend/src/pages/english_bot/styles/Footer.ts
+++ frontend/src/pages/english_bot/styles/Footer.ts
@@ -1,4 +1,4 @@
-export const footerStyle = {
+export const footerStyle: React.CSSProperties = {
     'position': 'fixed',
     'bottom': 0,
     'width': '100%',
@@ -8,13 +8,13 @@ export const footerStyle = {
     'marginTop': '5px',
 }
 
-export const leftStyle = {
+export const leftStyle: React.CSSProperties = {
     'position': 'absolute',/*←絶対位置*/
     'left': '0px',
     'lineHeight': '30px',
 }
 
-export const rightStyle = {
+export const rightStyle: React.CSSProperties = {
     'position': 'absolute',/*←絶対位置*/
     'right': '10px',
     'lineHeight': '30px',
