a1e54c728ff04e2b309578cfcfaa41c571e284e3
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2022-12-11 21:47:37 +0900

    パラメータ内の単語名を取得

--- frontend/src/pages/english_bot/pages/EnglishBotWordDetailPage.tsx
+++ frontend/src/pages/english_bot/pages/EnglishBotWordDetailPage.tsx
@@ -1,8 +1,12 @@
 import { Container } from "@mui/material";
 import React from "react";
+import { useParams } from "react-router-dom";
 import EnglishBotLayout from "../components/EnglishBotLayout";
 
 export default function EnglishBotWordDetailPage() {
+
+    // get word name from url
+    const { name } = useParams();
 
     const contents = () => {
         return (
