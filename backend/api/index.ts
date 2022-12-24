d155ee2d109f8802efe7f64b73ac0a6acb28ef40
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2022-12-24 12:27:59 +0900

    indexも

--- backend/api/index.ts
+++ backend/api/index.ts
@@ -1,66 +1,67 @@
-var express = require('express')
-var router = express.Router()
+import express from 'express';
+const router = express.Router()
 
-const logger = require('../common/Logger').getBackendLogger()
+import { getBackendLogger } from "../common/Logger";
+const logger = getBackendLogger()
 
-const QuizFileService = require('../services/QuizFileService')
-const CategoryService = require('../services/CategoryService')
-const QuizService = require('../services/QuizService')
-const S3Service = require('../aws/S3Service')
+import QuizFileService from "../services/QuizFileService";
+import { getAccuracyRateByCategoryService, getCategoryListService, replaceAllCategoryService } from "../services/CategoryService";
+import QuizService from "../services/QuizService";
+import S3Service from "../aws/S3Service";
 
-const PartsofSpeechService = require('../services/english/PartsofSpeechService')
-const WordService = require('../services/english/WordService')
+import PartsofSpeechService from "../services/english/PartsofSpeechService";
+import WordService from "../services/english/WordService";
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
-  res.send('Welcome to Quizzer!!')
+router.get("/", function (req, res, next) {
+  res.send("Welcome to Quizzer!!")
 })
 
-router.get('/namelist', function (req, res) {
+router.get("/namelist", function (req, res) {
   QuizFileService.getQuizFileList()
-    .then((result) => {
-      logger.debug('/namelist')
+    .then((result:any) => {
+      logger.debug("/namelist")
       res.status(200).send(result)
     })
-    .catch((error) => {
-      logger.error('/namelist')
+    .catch((error:any) => {
+      logger.error("/namelist")
       logger.error(error)
       res.status(500).send(error)
     })
 })
 
-router.post('/get_category', function (req, res) {
-  CategoryService.getCategoryList(req.body.file_num)
-    .then((result) => {
-      logger.debug('/get_category')
+router.post("/get_category", function (req, res) {
+  getCategoryListService(req.body.file_num)
+    .then((result:any) => {
+      logger.debug("/get_category")
       res.status(200).send(result)
     })
-    .catch((error) => {
-      logger.error('/get_category')
+    .catch((error:any) => {
+      logger.error("/get_category")
       logger.error(error)
       res.status(500).send(error)
     })
 })
 
-router.post('/get_quiz', function (req, res) {
+router.post("/get_quiz", function (req, res) {
   QuizService.getQuiz(req.body.file_num, req.body.quiz_num)
-    .then((result) => {
+    .then((result:any) => {
       if (result.length > 0) {
-        logger.debug('/get_quiz')
+        logger.debug("/get_quiz")
         res.status(200).send(result)
       } else {
-        logger.error('/get_quiz')
-        logger.error('404 Not Found')
+        logger.error("/get_quiz")
+        logger.error("404 Not Found")
         res.status(404).send(result)
       }
     })
-    .catch((error) => {
+    .catch((error:any) => {
       res.status(500).send(error)
       logger.error(error)
     })
 })
 
-router.post('/random', function (req, res) {
+router.post("/random", function (req, res) {
   QuizService.getRandomQuiz(
     req.body.file_num,
     req.body.min_rate,
@@ -68,106 +69,106 @@ router.post('/random', function (req, res) {
     req.body.category,
     req.body.checked
   )
-    .then((result) => {
+    .then((result:any) => {
       if (result.length > 0) {
-        logger.debug('/random')
+        logger.debug("/random")
         res.status(200).send(result)
       } else {
-        logger.error('/random')
-        logger.error('404 Not Found')
+        logger.error("/random")
+        logger.error("404 Not Found")
         res.status(404).send(result)
       }
     })
-    .catch((error) => {
+    .catch((error:any) => {
       res.status(500).send(error)
       logger.error(error)
     })
 })
 
-router.post('/worst_rate', function (req, res) {
+router.post("/worst_rate", function (req, res) {
   QuizService.getWorstRateQuiz(
     req.body.file_num,
     req.body.category,
     req.body.checked
   )
-    .then((result) => {
+    .then((result:any) => {
       if (result.length > 0) {
-        logger.debug('/worst_rate')
+        logger.debug("/worst_rate")
         res.status(200).send(result)
       } else {
-        logger.info('/worst_rate')
+        logger.info("/worst_rate")
         res.status(404).send(result)
       }
     })
-    .catch((error) => {
-      logger.error('/worst_rate')
+    .catch((error:any) => {
+      logger.error("/worst_rate")
       logger.error(error)
       res.status(500).send(error)
     })
 })
 
-router.post('/minimum_clear', function (req, res) {
+router.post("/minimum_clear", function (req, res) {
   QuizService.getMinimumClearQuiz(
     req.body.file_num,
     req.body.category,
     req.body.checked
   )
-    .then((result) => {
+    .then((result:any) => {
       if (result.length > 0) {
-        logger.debug('/minimum_clear')
+        logger.debug("/minimum_clear")
         res.status(200).send(result)
       } else {
-        logger.info('/minimum_clear')
+        logger.info("/minimum_clear")
         res.status(404).send(result)
       }
     })
-    .catch((error) => {
-      logger.error('/minimum_clear')
+    .catch((error:any) => {
+      logger.error("/minimum_clear")
       logger.error(error)
       res.status(500).send(error)
     })
 })
 
-router.post('/correct', function (req, res) {
+router.post("/correct", function (req, res) {
   QuizService.correctRegister(req.body.file_num, req.body.quiz_num)
-    .then((result) => {
-      logger.debug('/correct')
+    .then((result:any) => {
+      logger.debug("/correct")
       res.status(200).send(result)
     })
-    .catch((error) => {
-      logger.error('/correct')
+    .catch((error:any) => {
+      logger.error("/correct")
       logger.error(error)
       res.status(500).send(error)
     })
 })
 
-router.post('/incorrect', function (req, res) {
+router.post("/incorrect", function (req, res) {
   QuizService.incorrectRegister(req.body.file_num, req.body.quiz_num)
-    .then((result) => {
-      logger.debug('/incorrect')
+    .then((result:any) => {
+      logger.debug("/incorrect")
       res.status(200).send(result)
     })
-    .catch((error) => {
-      logger.error('/incorrect')
+    .catch((error:any) => {
+      logger.error("/incorrect")
       logger.error(error)
       res.status(500).send(error)
     })
 })
 
-router.post('/add', function (req, res) {
+router.post("/add", function (req, res) {
   QuizService.addQuiz(req.body.file_num, req.body.data)
-    .then((result) => {
-      logger.debug('/add')
+    .then((result:any) => {
+      logger.debug("/add")
       res.status(200).send(result)
     })
-    .catch((error) => {
-      logger.error('/add')
+    .catch((error:any) => {
+      logger.error("/add")
       logger.error(error)
       res.status(500).send(error)
     })
 })
 
-router.post('/edit', function (req, res) {
+router.post("/edit", function (req, res) {
   QuizService.editQuiz(
     req.body.file_num,
     req.body.quiz_num,
@@ -176,18 +177,18 @@ router.post('/edit', function (req, res) {
     req.body.category,
     req.body.img_file
   )
-    .then((result) => {
-      logger.debug('/edit')
+    .then((result:any) => {
+      logger.debug("/edit")
       res.status(200).send(result)
     })
-    .catch((error) => {
-      logger.error('/edit')
+    .catch((error:any) => {
+      logger.error("/edit")
       logger.error(error)
       res.status(500).send(error)
     })
 })
 
-router.post('/search', function (req, res) {
+router.post("/search", function (req, res) {
   QuizService.searchQuiz(
     req.body.file_num,
     req.body.min_rate,
@@ -197,87 +198,87 @@ router.post('/search', function (req, res) {
     req.body.query,
     req.body.cond
   )
-    .then((result) => {
+    .then((result:any) => {
       if (result.length > 0) {
-        logger.debug('/search')
+        logger.debug("/search")
         res.status(200).send(result)
       } else {
-        logger.error('/search')
-        logger.error('404 Not Found')
+        logger.error("/search")
+        logger.error("404 Not Found")
         res.status(404).send(result)
       }
     })
-    .catch((error) => {
+    .catch((error:any) => {
       logger.error(error)
       res.status(500).send(error)
     })
 })
 
-router.post('/delete', function (req, res) {
+router.post("/delete", function (req, res) {
   QuizService.deleteQuiz(req.body.file_num, req.body.quiz_num)
-    .then((result) => {
-      logger.debug('/delete')
+    .then((result:any) => {
+      logger.debug("/delete")
       res.status(200).send(result)
     })
-    .catch((error) => {
-      logger.error('/delete')
+    .catch((error:any) => {
+      logger.error("/delete")
       logger.error(error)
       res.status(500).send(error)
     })
 })
 
-router.post('/integrate', function (req, res) {
+router.post("/integrate", function (req, res) {
   QuizService.integrateQuiz(
     req.body.pre_file_num,
     req.body.pre_quiz_num,
     req.body.post_file_num,
     req.body.post_quiz_num
   )
-    .then((result) => {
-      logger.debug('/integrate')
+    .then((result:any) => {
+      logger.debug("/integrate")
       res.status(200).send(result)
     })
-    .catch((error) => {
-      logger.error('/integrate')
+    .catch((error:any) => {
+      logger.error("/integrate")
       logger.error(error)
       res.status(500).send(error)
     })
 })
 
-router.post('/category/renewal', function (req, res) {
-  CategoryService.replaceAllCategory(req.body.file_num)
-    .then((result) => {
-      logger.debug('/category/renewal')
+router.post("/category/renewal", function (req, res) {
+  replaceAllCategoryService(req.body.file_num)
+    .then((result:any) => {
+      logger.debug("/category/renewal")
       res.status(200).send(result)
     })
-    .catch((error) => {
-      logger.error('/category/renewal')
+    .catch((error:any) => {
+      logger.error("/category/renewal")
       logger.error(error)
       res.status(500).send(error)
     })
 })
 
-router.post('/category/accuracy_rate', function (req, res) {
-  CategoryService.getAccuracyRateByCategory(req.body.file_num)
-    .then((result) => {
-      logger.debug('/category/accuracy_rate')
+router.post("/category/accuracy_rate", function (req, res) {
+  getAccuracyRateByCategoryService(req.body.file_num)
+    .then((result:any) => {
+      logger.debug("/category/accuracy_rate")
       res.status(200).send(result)
     })
-    .catch((error) => {
-      logger.error('/category/accuracy_rate')
+    .catch((error:any) => {
+      logger.error("/category/accuracy_rate")
       logger.error(error)
       res.status(500).send(error)
     })
 })
 
-router.post('/upload', (req, res) => {
+router.post("/upload", (req, res) => {
   S3Service.upload(req.body.params)
-    .then((url) => {
-      logger.debug('/upload')
+    .then((url:any) => {
+      logger.debug("/upload")
       res.json({ url: url })
     })
-    .catch((error) => {
-      logger.error('/upload')
+    .catch((error:any) => {
+      logger.error("/upload")
       logger.error(error)
     })
 })
@@ -286,14 +287,14 @@ router.post('/upload', (req, res) => {
 //以下は英語版
 /////////////////
 
-router.get('/english/partsofspeech', function (req, res) {
+router.get("/english/partsofspeech", function (req, res) {
   PartsofSpeechService.getPartsofSpeech()
-    .then((result) => {
-      logger.debug('/english/partsofspeech')
+    .then((result:any) => {
+      logger.debug("/english/partsofspeech")
       res.status(200).send(result)
     })
-    .catch((error) => {
-      logger.error('/english/partsofspeech')
+    .catch((error:any) => {
+      logger.error("/english/partsofspeech")
       logger.error(error)
       res.status(500).send(error)
     })
@@ -306,18 +307,18 @@ router.get('/english/partsofspeech', function (req, res) {
 //  }
 //  ・・・
 // ]
-router.post('/english/word/add', function (req, res) {
+router.post("/english/word/add", function (req, res) {
   WordService.addWordAndMean(
     req.body.wordName,
     req.body.pronounce,
     req.body.meanArrayData
   )
-    .then((result) => {
-      logger.debug('/english/word/add')
+    .then((result:any) => {
+      logger.debug("/english/word/add")
       res.status(200).send(result)
     })
-    .catch((error) => {
-      logger.error('/english/word/add')
+    .catch((error:any) => {
+      logger.error("/english/word/add")
       logger.error(error)
       res.status(500).send(error)
     })
@@ -329,14 +330,14 @@ router.post('/english/word/add', function (req, res) {
  *  wordName: 単語名
  * }
  */
-router.post('/english/word/search', function (req, res) {
+router.post("/english/word/search", function (req, res) {
   WordService.searchWord(req.body.wordName)
-    .then((result) => {
-      logger.debug('/english/word/search')
+    .then((result:any) => {
+      logger.debug("/english/word/search")
       res.status(200).send(result)
     })
-    .catch((error) => {
-      logger.error('/english/word/search')
+    .catch((error:any) => {
+      logger.error("/english/word/search")
       logger.error(error)
       res.status(500).send(error)
     })
@@ -348,14 +349,14 @@ router.post('/english/word/search', function (req, res) {
  *  wordName: 単語名
  * }
  */
-router.post('/english/word/mean', function (req, res) {
+router.post("/english/word/mean", function (req, res) {
   WordService.getWordMean(req.body.wordName)
-    .then((result) => {
-      logger.debug('/english/word/mean')
+    .then((result:any) => {
+      logger.debug("/english/word/mean")
       res.status(200).send(result)
     })
-    .catch((error) => {
-      logger.error('/english/word/mean')
+    .catch((error:any) => {
+      logger.error("/english/word/mean")
       logger.error(error)
       res.status(500).send(error)
     })
