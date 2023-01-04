00eba0f4154ea06e94f26ff51b88d4219ab73af4
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2023-01-04 22:23:02 +0900

    TopPage　関数化

--- frontend/src/pages/quizzer/TopPage.tsx
+++ frontend/src/pages/quizzer/TopPage.tsx
@@ -7,30 +7,28 @@ const buttonStyle = {
   height: '100px'
 }
 
-export default class TopPage extends React.Component {
-  render() {
-    return (
-      <Container>
-        <h1>WAT Quizzer</h1>
-        <Button
-          style={buttonStyle}
-          variant="contained"
-          size="large"
-          color="primary"
-          href="/selectquiz"
-        >
-          Quizzer
-        </Button>
-        <Button
-          style={buttonStyle}
-          variant="contained"
-          size="large"
-          color="secondary"
-          href="/english/top"
-        >
-          English Quiz Bot
-        </Button>
-      </Container>
-    )
-  }
+export default function TopPage() {
+  return (
+    <Container>
+      <h1>WAT Quizzer</h1>
+      <Button
+        style={buttonStyle}
+        variant="contained"
+        size="large"
+        color="primary"
+        href="/selectquiz"
+      >
+        Quizzer
+      </Button>
+      <Button
+        style={buttonStyle}
+        variant="contained"
+        size="large"
+        color="secondary"
+        href="/english/top"
+      >
+        English Quiz Bot
+      </Button>
+    </Container>
+  )
 }
