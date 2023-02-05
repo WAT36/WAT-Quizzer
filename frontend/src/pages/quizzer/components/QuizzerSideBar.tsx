4f222f66dbf48499b14c0f2c0dbd715c91c812c3
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2023-02-05 13:02:38 +0900

    QuizzerSideBar mui化

--- frontend/src/pages/quizzer/components/QuizzerSideBar.tsx
+++ frontend/src/pages/quizzer/components/QuizzerSideBar.tsx
@@ -1,13 +1,11 @@
 import React from 'react'
 import { useRecoilState } from 'recoil'
-import Drawer from '@material-ui/core/Drawer'
-import List from '@material-ui/core/List'
-import ListItem from '@material-ui/core/ListItem'
 import { Link } from 'react-router-dom'
 
 import { sideBarContents, toggleDrawer } from '../utils/SideBar'
 import { isOpenState } from '../../../atoms/SideBar'
 import { drawerStyle } from '../../../styles/SideBar'
+import { Drawer, List, ListItem } from '@mui/material'
 
 export default function QuizzerSideBar() {
   const [sidebarState, setSidebarState] = useRecoilState(isOpenState)
