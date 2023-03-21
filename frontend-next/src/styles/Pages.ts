2812e0b8398d391c4a8d7fb58e76b59e29886f6e
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2023-03-21 16:08:48 +0900

    とりあえずトップページだけ持ってきた（背景変）

---
+++ frontend-next/src/styles/Pages.ts
@@ -0,0 +1,50 @@
+export const messageBoxStyle = {
+  margin: '10px 0px 20px',
+  borderStyle: 'none'
+};
+
+export const buttonStyle = {
+  margin: '10px'
+};
+
+export const topButtonStyle = {
+  margin: '20px',
+  width: '100px',
+  height: '100px'
+};
+
+export const searchedTableStyle = {
+  height: 600,
+  width: '100%'
+};
+
+export const groupStyle = {
+  border: 'solid thin lightgray',
+  borderRadius: '5px',
+  margin: '10px 0px',
+  padding: '0px 5px',
+  alignItems: 'center'
+};
+
+export const dropzoneStyle = {
+  backgroundColor: '#f4f4f4',
+  margin: '10px',
+  padding: '10px',
+  border: '#ddd dashed 5px',
+  minHeight: '200px',
+  textAlign: 'center' as 'center'
+};
+
+export const paperStyle = {
+  width: '40%',
+  float: 'left' as 'left',
+  margin: '5px'
+};
+
+export const typoStyles = {
+  fontSize: 14
+};
+
+const adjustedSpaceStyle = {
+  height: '40px'
+};
