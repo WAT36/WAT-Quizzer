8e365daf49716094bac317c6323f2a7ff272026b
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2023-03-25 00:07:36 +0900

    deleteQuiz 移し

--- frontend-next/src/pages/quizzer/utils/SideBar.ts
+++ frontend-next/src/pages/quizzer/utils/SideBar.ts
@@ -5,7 +5,7 @@ export const sideBarContents = [
   { name: '問題追加', link: '/quizzer/addQuiz' },
   { name: '問題編集', link: '/quizzer/editQuiz' },
   { name: '問題検索', link: '/quizzer/serachQuiz' },
-  { name: '問題削除', link: '/' },
+  { name: '問題削除', link: '/quizzer/deleteQuiz' },
   { name: 'カテゴリ別正解率表示', link: '/' },
   { name: '画像アップロード', link: '/' }
 ];
