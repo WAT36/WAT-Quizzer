2cfddea148e282c538660bb2415a4f857edbc3ad
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2023-04-23 02:34:41 +0900

    とりあえず Database繋げる用のファイル追加（まだ未確認）

--- database/initdb.d/init.sql
+++ database/initdb.d/init.sql
@@ -1,74 +1,80 @@
 CREATE DATABASE IF NOT EXISTS quiz_db;
+
 USE quiz_db;
 
-CREATE TABLE IF NOT EXISTS quiz
-(
-  file_num INT NOT NULL,
-  quiz_num INT NOT NULL,
-  quiz_sentense VARCHAR(256) NOT NULL,
-  answer VARCHAR(256) NOT NULL,
-  clear_count INT DEFAULT 0,
-  fail_count INT DEFAULT 0,
-  category VARCHAR(256),
-  img_file VARCHAR(128),
-  checked BOOLEAN DEFAULT 0,
-  deleted BOOLEAN DEFAULT 0,
-  PRIMARY KEY(file_num,quiz_num)
-) DEFAULT CHARACTER
-  SET=utf8;
+CREATE TABLE
+    IF NOT EXISTS quiz (
+        file_num INT NOT NULL,
+        quiz_num INT NOT NULL,
+        quiz_sentense VARCHAR(256) NOT NULL,
+        answer VARCHAR(256) NOT NULL,
+        clear_count INT DEFAULT 0,
+        fail_count INT DEFAULT 0,
+        category VARCHAR(256),
+        img_file VARCHAR(128),
+        checked BOOLEAN DEFAULT 0,
+        deleted BOOLEAN DEFAULT 0,
+        PRIMARY KEY(file_num, quiz_num)
+    ) DEFAULT CHARACTER SET = utf8;
 
-CREATE TABLE IF NOT EXISTS quiz_file
-(
-  file_num INT NOT NULL PRIMARY KEY,
-  file_name VARCHAR(256) NOT NULL,
-  file_nickname VARCHAR(256) NOT NULL
-) DEFAULT CHARACTER
-  SET=utf8;
+CREATE TABLE
+    IF NOT EXISTS quiz_file (
+        file_num INT NOT NULL PRIMARY KEY,
+        file_name VARCHAR(256) NOT NULL,
+        file_nickname VARCHAR(256) NOT NULL
+    ) DEFAULT CHARACTER SET = utf8;
 
-CREATE TABLE IF NOT EXISTS category
-(
-  file_num INT NOT NULL,
-  category VARCHAR(256) NOT NULL
-) DEFAULT CHARACTER
-  SET=utf8;
+CREATE TABLE
+    IF NOT EXISTS category (
+        file_num INT NOT NULL,
+        category VARCHAR(256) NOT NULL,
+        PRIMARY KEY(file_num, category)
+    ) DEFAULT CHARACTER SET = utf8;
 
 DROP VIEW IF EXISTS quiz_view;
-CREATE VIEW quiz_view AS 
-SELECT 
-  file_num,
-  quiz_num,
-  quiz_sentense,
-  answer,
-  clear_count,
-  fail_count,
-  category,
-  img_file,
-  checked,
-  deleted,
-  CASE WHEN (clear_count + fail_count = 0) THEN 0
-    ELSE 100 * clear_count / (clear_count + fail_count) 
-    END
-    AS accuracy_rate
-FROM quiz;
 
+CREATE VIEW QUIZ_VIEW AS 
+	SELECT
+	    file_num,
+	    quiz_num,
+	    quiz_sentense,
+	    answer,
+	    clear_count,
+	    fail_count,
+	    category,
+	    img_file,
+	    checked,
+	    deleted,
+	    CASE
+	        WHEN (clear_count + fail_count = 0) THEN 0
+	        ELSE 100 * clear_count / (clear_count + fail_count)
+	    END AS accuracy_rate
+	FROM
+QUIZ; 
 
 DROP VIEW IF EXISTS category_view;
-CREATE VIEW category_view AS 
-SELECT 
-  c.file_num as file_num,
-  c.category as c_category,
-  COUNT(*) as count,
-  SUM(clear_count) as sum_of_clear_count,
-  SUM(fail_count) as sum_of_fail_count,
-  CASE WHEN (SUM(clear_count) + SUM(fail_count) = 0) THEN 0
-    ELSE 100 * SUM(clear_count) / (SUM(clear_count) + SUM(fail_count) ) 
-    END
-    AS accuracy_rate
-FROM category as c 
-CROSS JOIN quiz as a 
-WHERE c.file_num = a.file_num 
-AND a.category LIKE concat('%',c.category,'%')
-AND a.deleted != 1
-GROUP BY file_num,c_category
-ORDER BY file_num,c_category;
 
+CREATE VIEW CATEGORY_VIEW AS 
+	SELECT
+	    c.file_num as file_num,
+	    c.category as c_category,
+	    COUNT(*) as count,
+	    SUM(clear_count) as sum_of_clear_count,
+	    SUM(fail_count) as sum_of_fail_count,
+	    CASE
+	        WHEN (
+	            SUM(clear_count) + SUM(fail_count) = 0
+	        ) THEN 0
+	        ELSE 100 * SUM(clear_count) / (
+	            SUM(clear_count) + SUM(fail_count)
+	        )
+	    END AS accuracy_rate
+	FROM category as c
+	    CROSS JOIN quiz as a
+	WHERE
+	    c.file_num = a.file_num
+	    AND a.category LIKE concat('%', c.category, '%')
+	    AND a.deleted != 1
+	GROUP BY file_num, c_category
+	ORDER BY file_num, c
+C_CATEGORY; 
