20ff548a8e6660a845d31cf6db22b656f6a7032d
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2023-06-10 22:33:47 +0900

    test用　hello world APIのみにして実施

--- backend-nest/src/app.module.ts
+++ backend-nest/src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { QuizModule } from './quiz/quiz.module';
+//import { QuizModule } from './quiz/quiz.module';
 
 @Module({
-  imports: [QuizModule],
+  imports: [
+    //QuizModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
