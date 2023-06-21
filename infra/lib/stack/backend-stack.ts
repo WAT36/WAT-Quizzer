264356757e9d388e12b8beedec645d8423366426
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2023-06-21 19:01:57 +0900

    nest用バケット作成

--- infra/lib/stack/backend-stack.ts
+++ infra/lib/stack/backend-stack.ts
@@ -1,4 +1,5 @@
 import * as cdk from 'aws-cdk-lib'
+import * as s3 from 'aws-cdk-lib/aws-s3'
 import { Construct } from 'constructs'
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
@@ -12,5 +13,10 @@ export class BackendStack extends cdk.Stack {
     // const queue = new sqs.Queue(this, 'InfraQueue', {
     //   visibilityTimeout: cdk.Duration.seconds(300)
     // });
+
+    // S3 Bucket for NestJS Lambda
+    const bucket = new s3.Bucket(this, 'nestJSLambdaBucket', {
+      bucketName: 'dev-nestjsapi-lambda-layer-bucket'
+    })
   }
 }
