4b3992d54ec601cbdde87b28a017fdfcd32daec1
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2023-07-01 22:55:15 +0900

    cognitoユーザープール作成

--- infra/lib/stack/auth-stack.ts
+++ infra/lib/stack/auth-stack.ts
@@ -1,6 +1,7 @@
 import * as cdk from 'aws-cdk-lib'
 import { Construct } from 'constructs'
 import * as dotenv from 'dotenv'
+import * as cognito from 'aws-cdk-lib/aws-cognito'
 
 dotenv.config()
 
@@ -16,5 +17,34 @@ export class AuthStack extends cdk.Stack {
     })
 
     const { region, accountId } = new cdk.ScopedAws(this)
+
+    // cognito userpool
+    const userPool = new cognito.UserPool(this, `${props.env}QuizzerUserPool`, {
+      signInAliases: {
+        username: true,
+        phone: false,
+        email: false,
+        preferredUsername: true
+      },
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      signInCaseSensitive: true,
+      accountRecovery: cognito.AccountRecovery.NONE,
+      selfSignUpEnabled: false,
+      email: cognito.UserPoolEmail.withCognito()
+    })
+
+    // cognito domain
+    userPool.addDomain('userPoolDomain', {
+      cognitoDomain: { domainPrefix: process.env.COGNITO_DOMAIN || '' }
+    })
+
+    // cognito app client
+    userPool.addClient('userPoolAppClient', {
+      generateSecret: false,
+      oAuth: {
+        callbackUrls: [process.env.FRONT_CALLBACK_URL || ''],
+        logoutUrls: [process.env.LOGOUT_URL || '']
+      }
+    })
   }
 }
