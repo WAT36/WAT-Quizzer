e51894f5c2bd9bb3694fa9caddb83cc6e38d31d7
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2023-07-10 20:53:03 +0900

    Lambda@Edge関数変更（使用ライブラリも）

---
+++ infra/lib/service/lambda-edge/cognito-at-edge/index.js
@@ -0,0 +1,14 @@
+const { Authenticator } = require('cognito-at-edge')
+const fs = require('fs')
+
+const value = JSON.parse(fs.readFileSync('./ids.json', 'utf8'))
+const authenticator = new Authenticator({
+  region: value.REGION,
+  userPoolId: value.USERPOOL_ID,
+  userPoolAppId: value.USERPOOL_APPCLIENT_ID,
+  userPoolDomain: value.USERPOOL_COGNITO_DOMAIN
+})
+
+exports.handler = async (request) => {
+  return authenticator.handle(request)
+}
