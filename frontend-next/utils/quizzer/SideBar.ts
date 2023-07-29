afb994d3f742a3f363a2f8521056abf258360567
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2023-07-30 02:08:33 +0900

    サイドバーリンクエラー修正

--- frontend-next/utils/quizzer/SideBar.ts
+++ frontend-next/utils/quizzer/SideBar.ts
@@ -1,13 +1,14 @@
 // サイドバーのコンテンツ
+const urlEnd = process.env.NEXT_PUBLIC_URL_END || '';
 export const sideBarContents = [
-  { name: 'トップ', link: process.env.NEXT_PUBLIC_URL_END },
-  { name: '問題出題', link: '/quizzer' + process.env.NEXT_PUBLIC_URL_END },
-  { name: '問題追加', link: '/quizzer/addQuiz' + process.env.NEXT_PUBLIC_URL_END },
-  { name: '問題編集', link: '/quizzer/editQuiz' + process.env.NEXT_PUBLIC_URL_END },
-  { name: '問題検索', link: '/quizzer/searchQuiz' + process.env.NEXT_PUBLIC_URL_END },
-  { name: '問題削除', link: '/quizzer/deleteQuiz' + process.env.NEXT_PUBLIC_URL_END },
-  { name: 'カテゴリ別正解率表示', link: '/quizzer/accuracyRateGraph' + process.env.NEXT_PUBLIC_URL_END },
-  { name: '画像アップロード', link: '/quizzer/imageUpload' + process.env.NEXT_PUBLIC_URL_END }
+  { name: 'トップ', link: urlEnd },
+  { name: '問題出題', link: '/quizzer' + urlEnd },
+  { name: '問題追加', link: '/quizzer/addQuiz' + urlEnd },
+  { name: '問題編集', link: '/quizzer/editQuiz' + urlEnd },
+  { name: '問題検索', link: '/quizzer/searchQuiz' + urlEnd },
+  { name: '問題削除', link: '/quizzer/deleteQuiz' + urlEnd },
+  { name: 'カテゴリ別正解率表示', link: '/quizzer/accuracyRateGraph' + urlEnd },
+  { name: '画像アップロード', link: '/quizzer/imageUpload' + urlEnd }
 ];
 
 // サイドバー開閉
