b9e4a13f774f0fe40eb1a1ff7f16c7835924c0d0
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2023-07-29 22:47:59 +0900

    検索画面へのサイドバーリンク　スペルミス修正

--- frontend-next/utils/quizzer/SideBar.ts
+++ frontend-next/utils/quizzer/SideBar.ts
@@ -4,7 +4,7 @@ export const sideBarContents = [
   { name: '問題出題', link: '/quizzer' + process.env.NEXT_PUBLIC_URL_END },
   { name: '問題追加', link: '/quizzer/addQuiz' + process.env.NEXT_PUBLIC_URL_END },
   { name: '問題編集', link: '/quizzer/editQuiz' + process.env.NEXT_PUBLIC_URL_END },
-  { name: '問題検索', link: '/quizzer/serachQuiz' + process.env.NEXT_PUBLIC_URL_END },
+  { name: '問題検索', link: '/quizzer/searchQuiz' + process.env.NEXT_PUBLIC_URL_END },
   { name: '問題削除', link: '/quizzer/deleteQuiz' + process.env.NEXT_PUBLIC_URL_END },
   { name: 'カテゴリ別正解率表示', link: '/quizzer/accuracyRateGraph' + process.env.NEXT_PUBLIC_URL_END },
   { name: '画像アップロード', link: '/quizzer/imageUpload' + process.env.NEXT_PUBLIC_URL_END }
