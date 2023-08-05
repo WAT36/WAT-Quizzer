042e61c4a86539cfa0d98b8eb0b2312d46869e09
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2023-08-05 21:15:03 +0900

    英単語追加画面に　出典追加の項目作成　close #123

--- frontend-next/src/pages/englishBot/addWord.tsx
+++ frontend-next/src/pages/englishBot/addWord.tsx
@@ -30,11 +30,12 @@ export default function EnglishBotAddWordPage() {
   const [message, setMessage] = useState<string>('　');
   const [messageColor, setMessageColor] = useState<string>('common.black');
   const [posList, setPosList] = useState<JSX.Element[]>([]);
+  const [sourceList, setSourceList] = useState<JSX.Element[]>([]);
   const [meanRowList, setMeanRowList] = useState<meanOfAddWordDto[]>([]);
   const [inputWord, setInputWord] = useState<string>('');
 
   useEffect(() => {
-    getPartOfSpeechList();
+    Promise.all([getPartOfSpeechList(), getSourceList()]);
   }, []);
 
   const messeageClear = () => {
@@ -43,7 +44,7 @@ export default function EnglishBotAddWordPage() {
   };
 
   // 品詞リスト取得
-  const getPartOfSpeechList = () => {
+  const getPartOfSpeechList = async () => {
     setMessage('通信中...');
     setMessageColor('#d3d3d3');
     get('/english/partsofspeech', (data: any) => {
@@ -72,11 +73,45 @@ export default function EnglishBotAddWordPage() {
     });
   };
 
+  // 出典リスト取得
+  const getSourceList = async () => {
+    setMessage('通信中...');
+    setMessageColor('#d3d3d3');
+    get('/english/source', (data: any) => {
+      if (data.status === 200) {
+        data = data.body;
+        let gotSourceList = [];
+        for (var i = 0; i < data.length; i++) {
+          gotSourceList.push(
+            <MenuItem value={data[i].id} key={data[i].id}>
+              {data[i].name}
+            </MenuItem>
+          );
+        }
+        gotSourceList.push(
+          <MenuItem value={-2} key={-2}>
+            {'その他'}
+          </MenuItem>
+        );
+        setSourceList(gotSourceList);
+        setMessage('　');
+        setMessageColor('common.black');
+      } else {
+        setMessage('エラー:外部APIとの連携に失敗しました');
+        setMessageColor('error');
+      }
+    });
+  };
+
   // 列をステートに追加
   const setTableRow = () => {
     const copyMeanRowList = [...meanRowList];
     copyMeanRowList.push({
       pos: {
+        id: -1,
+        name: undefined
+      },
+      source: {
         id: -1,
         name: undefined
       },
@@ -128,6 +163,49 @@ export default function EnglishBotAddWordPage() {
     );
   };
 
+  // 出典プルダウン表示、「その他」だったら入力用テキストボックスを出す
+  const displaySourceInput = (i: number) => {
+    const sourceInput =
+      meanRowList[i] && meanRowList[i].source.id === -2 ? (
+        <>
+          <TextField
+            id="input-pos-01"
+            label="出典"
+            variant="outlined"
+            key={i}
+            sx={{ width: 1 }}
+            onChange={(e) => {
+              inputSource(e.target.value, i);
+            }}
+          />
+        </>
+      ) : (
+        <></>
+      );
+
+    return (
+      <>
+        <Select
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+          defaultValue={-1}
+          label="source"
+          key={i}
+          sx={{ width: 1 }}
+          onChange={(e) => {
+            changeSourceSelect(String(e.target.value), i);
+          }}
+        >
+          <MenuItem value={-1} key={-1}>
+            選択なし
+          </MenuItem>
+          {sourceList}
+        </Select>
+        {sourceInput}
+      </>
+    );
+  };
+
   // 列を追加
   const addRow = () => {
     messeageClear();
@@ -142,6 +220,10 @@ export default function EnglishBotAddWordPage() {
           <TableCell>
             <InputLabel id="demo-simple-select-label"></InputLabel>
             {displayPosInput(index)}
+          </TableCell>
+          <TableCell>
+            <InputLabel id="demo-simple-select-label"></InputLabel>
+            {displaySourceInput(index)}
           </TableCell>
           <TableCell>
             <TextField
@@ -169,6 +251,9 @@ export default function EnglishBotAddWordPage() {
           pos: {
             id: -1
           },
+          source: {
+            id: -1
+          },
           mean: undefined
         });
       }
@@ -178,6 +263,7 @@ export default function EnglishBotAddWordPage() {
         id: Number(value),
         name: Number(value) === -2 ? copyMeanRowList[i].pos.name : undefined
       },
+      source: copyMeanRowList[i].source,
       mean: copyMeanRowList[i].mean
     };
     setMeanRowList(copyMeanRowList);
@@ -189,6 +275,48 @@ export default function EnglishBotAddWordPage() {
     copyMeanRowList[i] = {
       pos: {
         id: copyMeanRowList[i].pos.id,
+        name: value
+      },
+      source: copyMeanRowList[i].source,
+      mean: copyMeanRowList[i].mean
+    };
+    setMeanRowList(copyMeanRowList);
+  };
+
+  // 出典プルダウン変更時の入力の更新
+  const changeSourceSelect = (value: string, i: number) => {
+    const copyMeanRowList = [...meanRowList];
+    if (i >= copyMeanRowList.length) {
+      while (i >= copyMeanRowList.length) {
+        copyMeanRowList.push({
+          pos: {
+            id: -1
+          },
+          source: {
+            id: -1
+          },
+          mean: undefined
+        });
+      }
+    }
+    copyMeanRowList[i] = {
+      pos: copyMeanRowList[i].pos,
+      source: {
+        id: Number(value),
+        name: Number(value) === -2 ? copyMeanRowList[i].source.name : undefined
+      },
+      mean: copyMeanRowList[i].mean
+    };
+    setMeanRowList(copyMeanRowList);
+  };
+
+  // 出典入力時の処理
+  const inputSource = (value: string, i: number) => {
+    const copyMeanRowList = [...meanRowList];
+    copyMeanRowList[i] = {
+      pos: copyMeanRowList[i].pos,
+      source: {
+        id: copyMeanRowList[i].source.id,
         name: value
       },
       mean: copyMeanRowList[i].mean
@@ -205,12 +333,16 @@ export default function EnglishBotAddWordPage() {
           pos: {
             id: -1
           },
+          source: {
+            id: -1
+          },
           mean: undefined
         });
       }
     }
     copyMeanRowList[i] = {
       pos: copyMeanRowList[i].pos,
+      source: copyMeanRowList[i].source,
       mean: value
     };
     setMeanRowList(copyMeanRowList);
@@ -253,8 +385,10 @@ export default function EnglishBotAddWordPage() {
           if (currentValue.pos.id !== -1) {
             previousValue.push({
               partOfSpeechId: currentValue.pos.id,
+              sourceId: currentValue.source.id,
               meaning: currentValue.mean || '',
-              partOfSpeechName: currentValue.pos.name
+              partOfSpeechName: currentValue.pos.name,
+              sourceName: currentValue.source.name
             });
           }
           return previousValue;
@@ -282,6 +416,7 @@ export default function EnglishBotAddWordPage() {
           <TableHead>
             <TableRow>
               <TableCell sx={{ width: 200 }}>{'品詞'}</TableCell>
+              <TableCell sx={{ width: 200 }}>{'出典'}</TableCell>
               <TableCell>{'意味'}</TableCell>
             </TableRow>
           </TableHead>
--- frontend-next/src/pages/englishBot/dto/addWordDto.ts
+++ frontend-next/src/pages/englishBot/dto/addWordDto.ts
@@ -3,11 +3,17 @@ export interface meanOfAddWordDto {
     id: number;
     name?: string;
   };
+  source: {
+    id: number;
+    name?: string;
+  };
   mean: string | undefined;
 }
 
 export interface SendToAddWordApiData {
   partOfSpeechId: number;
+  sourceId: number;
   meaning: string;
   partOfSpeechName?: string;
+  sourceName?: string;
 }
