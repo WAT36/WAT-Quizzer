3abb8d83e021f58b6130ff8fe48cedf155761910
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2023-08-05 20:34:59 +0900

    sql prettier

--- database/initdb.d/english.sql
+++ database/initdb.d/english.sql
@@ -1,95 +1,87 @@
 CREATE DATABASE IF NOT EXISTS english_db;
+
 USE english_db;
 
-CREATE TABLE IF NOT EXISTS word
-(
-  id INT NOT NULL AUTO_INCREMENT,
-  name VARCHAR(256) NOT NULL,
-  pronounce VARCHAR(256) NOT NULL,
-  PRIMARY KEY(id) 
-) DEFAULT CHARACTER
-  SET=utf8;
+CREATE TABLE
+    IF NOT EXISTS word (
+        id INT NOT NULL AUTO_INCREMENT,
+        name VARCHAR(256) NOT NULL,
+        pronounce VARCHAR(256) NOT NULL,
+        PRIMARY KEY(id)
+    ) DEFAULT CHARACTER SET = utf8;
 
-CREATE TABLE IF NOT EXISTS partsofspeech
-(
-  id INT NOT NULL AUTO_INCREMENT,
-  name VARCHAR(256) NOT NULL,
-  PRIMARY KEY(id)
-) DEFAULT CHARACTER
-  SET=utf8;
+CREATE TABLE
+    IF NOT EXISTS partsofspeech (
+        id INT NOT NULL AUTO_INCREMENT,
+        name VARCHAR(256) NOT NULL,
+        PRIMARY KEY(id)
+    ) DEFAULT CHARACTER SET = utf8;
 
-CREATE TABLE IF NOT EXISTS mean
-(
-  id INT NOT NULL AUTO_INCREMENT,
-  word_id INT NOT NULL,
-  wordmean_id INT NOT NULL,
-  partsofspeech_id INT NOT NULL,
-  meaning VARCHAR(256) NOT NULL,
-  PRIMARY KEY(id),
-  UNIQUE wordmean_index (word_id,wordmean_id),
-  FOREIGN KEY word_id_foreign_key (word_id) REFERENCES word(id),
-  FOREIGN KEY partsofspeech_id_foreign_key (partsofspeech_id) REFERENCES partsofspeech(id)
-) DEFAULT CHARACTER
-  SET=utf8;
+CREATE TABLE
+    IF NOT EXISTS mean (
+        id INT NOT NULL AUTO_INCREMENT,
+        word_id INT NOT NULL,
+        wordmean_id INT NOT NULL,
+        partsofspeech_id INT NOT NULL,
+        meaning VARCHAR(256) NOT NULL,
+        PRIMARY KEY(id),
+        UNIQUE wordmean_index (word_id, wordmean_id),
+        FOREIGN KEY word_id_foreign_key (word_id) REFERENCES word(id),
+        FOREIGN KEY partsofspeech_id_foreign_key (partsofspeech_id) REFERENCES partsofspeech(id)
+    ) DEFAULT CHARACTER SET = utf8;
 
-CREATE TABLE IF NOT EXISTS example
-(
-  id INT NOT NULL AUTO_INCREMENT,
-  en_example_sentense VARCHAR(256) NOT NULL,
-  ja_example_sentense VARCHAR(256) NOT NULL,
-  PRIMARY KEY(id)
-) DEFAULT CHARACTER
-  SET=utf8;
+CREATE TABLE
+    IF NOT EXISTS example (
+        id INT NOT NULL AUTO_INCREMENT,
+        en_example_sentense VARCHAR(256) NOT NULL,
+        ja_example_sentense VARCHAR(256) NOT NULL,
+        PRIMARY KEY(id)
+    ) DEFAULT CHARACTER SET = utf8;
 
-CREATE TABLE IF NOT EXISTS mean_example
-(
-  example_sentense_id INT NOT NULL,
-  mean_id INT NOT NULL,
-  PRIMARY KEY(example_sentense_id,mean_id),
-  FOREIGN KEY example_sentense_id_foreign_key (example_sentense_id) REFERENCES example(id),
-  FOREIGN KEY mean_id_foreign_key (mean_id) REFERENCES mean(id)
-) DEFAULT CHARACTER
-  SET=utf8;
+CREATE TABLE
+    IF NOT EXISTS mean_example (
+        example_sentense_id INT NOT NULL,
+        mean_id INT NOT NULL,
+        PRIMARY KEY(example_sentense_id, mean_id),
+        FOREIGN KEY example_sentense_id_foreign_key (example_sentense_id) REFERENCES example(id),
+        FOREIGN KEY mean_id_foreign_key (mean_id) REFERENCES mean(id)
+    ) DEFAULT CHARACTER SET = utf8;
 
-CREATE TABLE IF NOT EXISTS idiom
-(
-  id INT NOT NULL AUTO_INCREMENT,
-  word_id INT NOT NULL,
-  name VARCHAR(256) NOT NULL,
-  PRIMARY KEY(id),
-  FOREIGN KEY word_id_foreign_key (word_id) REFERENCES word(id)
-) DEFAULT CHARACTER
-  SET=utf8;
+CREATE TABLE
+    IF NOT EXISTS idiom (
+        id INT NOT NULL AUTO_INCREMENT,
+        word_id INT NOT NULL,
+        name VARCHAR(256) NOT NULL,
+        PRIMARY KEY(id),
+        FOREIGN KEY word_id_foreign_key (word_id) REFERENCES word(id)
+    ) DEFAULT CHARACTER SET = utf8;
 
-CREATE TABLE IF NOT EXISTS idiom_mean
-(
-  id INT NOT NULL AUTO_INCREMENT,
-  idiom_id INT NOT NULL,
-  idiommean_id INT NOT NULL,
-  partsofspeech_id INT NOT NULL,
-  meaning VARCHAR(256) NOT NULL,
-  PRIMARY KEY(id),
-  UNIQUE idiommean_index (idiom_id,idiommean_id),
-  FOREIGN KEY idiom_id_foreign_key (idiom_id) REFERENCES idiom(id),
-  FOREIGN KEY partsofspeech_id_foreign_key (partsofspeech_id) REFERENCES partsofspeech(id)
-) DEFAULT CHARACTER
-  SET=utf8;
+CREATE TABLE
+    IF NOT EXISTS idiom_mean (
+        id INT NOT NULL AUTO_INCREMENT,
+        idiom_id INT NOT NULL,
+        idiommean_id INT NOT NULL,
+        partsofspeech_id INT NOT NULL,
+        meaning VARCHAR(256) NOT NULL,
+        PRIMARY KEY(id),
+        UNIQUE idiommean_index (idiom_id, idiommean_id),
+        FOREIGN KEY idiom_id_foreign_key (idiom_id) REFERENCES idiom(id),
+        FOREIGN KEY partsofspeech_id_foreign_key (partsofspeech_id) REFERENCES partsofspeech(id)
+    ) DEFAULT CHARACTER SET = utf8;
 
-CREATE TABLE IF NOT EXISTS source
-(
-  id INT NOT NULL AUTO_INCREMENT,
-  name VARCHAR(256) NOT NULL,
-  PRIMARY KEY(id)
-) DEFAULT CHARACTER
-  SET=utf8;
+CREATE TABLE
+    IF NOT EXISTS source (
+        id INT NOT NULL AUTO_INCREMENT,
+        name VARCHAR(256) NOT NULL,
+        PRIMARY KEY(id)
+    ) DEFAULT CHARACTER SET = utf8;
 
-CREATE TABLE IF NOT EXISTS mean_source
-(
-  source_id INT NOT NULL,
-  word_id INT NOT NULL,
-  mean_id INT NOT NULL,
-  PRIMARY KEY(source_id,word_id,mean_id),
-  FOREIGN KEY source_id_foreign_key (source_id) REFERENCES source(id),
-  FOREIGN KEY word_mean_id_foreign_key (word_id,mean_id) REFERENCES mean(word_id,wordmean_id)
-) DEFAULT CHARACTER
-  SET=utf8;
+CREATE TABLE
+    IF NOT EXISTS mean_source (
+        source_id INT NOT NULL,
+        word_id INT NOT NULL,
+        mean_id INT NOT NULL,
+        PRIMARY KEY(source_id, word_id, mean_id),
+        FOREIGN KEY source_id_foreign_key (source_id) REFERENCES source(id),
+        FOREIGN KEY word_mean_id_foreign_key (word_id, mean_id) REFERENCES mean(word_id, wordmean_id)
+    ) DEFAULT CHARACTER SET = utf8;
