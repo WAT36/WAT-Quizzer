af0f140ba4e3a3b4ad4ab998a8bff58655425e95
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2023-08-17 23:59:52 +0900

    単語詳細トップページ案（具体的にどうしようかは考えたい）

---
+++ frontend-next/src/pages/englishBot/detailWord/index.tsx
@@ -0,0 +1,82 @@
+import React, { useState } from 'react';
+
+import { get } from '../../../common/API';
+import EnglishBotLayout from '../components/EnglishBotLayout';
+import { buttonStyle, messageBoxStyle } from '../../../styles/Pages';
+import { Button, Card, CardContent, Container, FormControl, FormGroup, TextField, Typography } from '@mui/material';
+import { DataGrid, GridRowsProp } from '@mui/x-data-grid';
+
+export default function EnglishBotDetailWordPage() {
+  const [query, setQuery] = useState('');
+  const [searchResult, setSearchResult] = useState<GridRowsProp>([] as GridRowsProp);
+  const [message, setMessage] = useState({
+    message: '　',
+    messageColor: 'common.black'
+  });
+
+  const searchWord = () => {
+    if (!query || query === '') {
+      setMessage({ message: 'エラー:検索語句を入力して下さい', messageColor: 'error' });
+      return;
+    }
+
+    setMessage({ message: '通信中...', messageColor: '#d3d3d3' });
+    get(
+      '/english/word/search',
+      (data: any) => {
+        if (data.status === 200) {
+          const result = data.body?.wordData || [];
+          setSearchResult(result);
+          setMessage({
+            message: 'Success!!' + result.length + '問の問題を取得しました',
+            messageColor: 'success.light'
+          });
+        } else {
+          setMessage({
+            message: 'エラー:外部APIとの連携に失敗しました',
+            messageColor: 'error'
+          });
+        }
+      },
+      {
+        wordName: query
+      }
+    );
+  };
+
+  const contents = () => {
+    return (
+      <Container>
+        <h1>Detail Word</h1>
+        <Card variant="outlined" style={messageBoxStyle}>
+          <CardContent>
+            <Typography variant="h6" component="h6" color={message.messageColor}>
+              {message.message}
+            </Typography>
+          </CardContent>
+        </Card>
+
+        <FormGroup>
+          <FormControl>
+            <TextField
+              label="単語入力"
+              onChange={(e) => {
+                setQuery(e.target.value);
+              }}
+            />
+          </FormControl>
+        </FormGroup>
+
+        <Button style={buttonStyle} variant="contained" color="primary" onClick={(e) => searchWord()}>
+          検索
+        </Button>
+      </Container>
+    );
+  };
+
+  return (
+    <>
+      <EnglishBotLayout contents={contents()} />
+    </>
+  );
+}
--- frontend-next/utils/englishBot/SideBar.ts
+++ frontend-next/utils/englishBot/SideBar.ts
@@ -2,7 +2,8 @@
 export const sideBarContents = [
   { name: 'Top', link: '/englishBot' + process.env.NEXT_PUBLIC_URL_END },
   { name: 'Add Words', link: '/englishBot/addWord' + process.env.NEXT_PUBLIC_URL_END },
-  { name: 'Dictionary', link: '/englishBot/dictionary' + process.env.NEXT_PUBLIC_URL_END }
+  { name: 'Dictionary', link: '/englishBot/dictionary' + process.env.NEXT_PUBLIC_URL_END },
+  { name: 'Detail Word', link: '/englishBot/detailWord' + process.env.NEXT_PUBLIC_URL_END }
 ];
 
 // サイドバー開閉
