b09656121aca34756949756b7dd3711e504c2331
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2023-08-27 22:56:58 +0900

    入れ子構成少し修正

--- frontend-next/src/pages/englishBot/addExample.tsx
+++ frontend-next/src/pages/englishBot/addExample.tsx
@@ -2,7 +2,21 @@ import React, { useState } from 'react';
 
 import EnglishBotLayout from './components/EnglishBotLayout';
 import { messageBoxStyle } from '../../styles/Pages';
-import { Card, CardContent, Container, Typography } from '@mui/material';
+import { Button, Card, CardContent, CardHeader, Container, TextField, Typography } from '@mui/material';
+
+const cardContentStyle = {
+  display: 'flex',
+  width: '100%'
+};
+
+const inputTextBeforeButtonStyle = {
+  flex: 'auto'
+};
+
+const buttonAfterInputTextStyle = {
+  flex: 'none',
+  margin: '10px'
+};
 
 export default function EnglishBotAddExamplePage() {
   const [message, setMessage] = useState({
@@ -20,6 +34,42 @@ export default function EnglishBotAddExamplePage() {
             <Typography variant="h6" component="h6" color={message.messageColor}>
               {message.message}
             </Typography>
+          </CardContent>
+        </Card>
+
+        <Card variant="outlined">
+          <CardHeader title="例文追加" />
+          <CardContent>
+            <Card variant="outlined">
+              <CardHeader subheader="例文(英文)" />
+              <CardContent style={cardContentStyle}>
+                <TextField
+                  label="例文(英語)"
+                  variant="outlined"
+                  // onChange={(e) => {
+                  //   setFileName(e.target.value);
+                  // }}
+                  style={inputTextBeforeButtonStyle}
+                />
+                {/* <Button variant="contained" style={buttonAfterInputTextStyle} onClick={(e) => addFile()}>
+                  追加
+                </Button> */}
+              </CardContent>
+              <CardHeader subheader="例文(和訳)" />
+              <CardContent style={cardContentStyle}>
+                <TextField
+                  label="例文(和訳)"
+                  variant="outlined"
+                  // onChange={(e) => {
+                  //   setFileName(e.target.value);
+                  // }}
+                  style={inputTextBeforeButtonStyle}
+                />
+                {/* <Button variant="contained" style={buttonAfterInputTextStyle} onClick={(e) => addFile()}>
+                  追加
+                </Button> */}
+              </CardContent>
+            </Card>
           </CardContent>
         </Card>
       </Container>
