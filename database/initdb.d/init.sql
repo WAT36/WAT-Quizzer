bbc926751b5d0815d76549021433f9c8d72bb09a
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2023-09-03 01:51:38 +0900

    啓発本リストテーブルと格言リストテーブルを追加

--- database/initdb.d/init.sql
+++ database/initdb.d/init.sql
@@ -49,6 +49,28 @@ CREATE TABLE
         PRIMARY KEY(id)
     ) DEFAULT CHARACTER SET = utf8;
 
+CREATE TABLE
+    IF NOT EXISTS selphelp_book (
+        id INT NOT NULL AUTO_INCREMENT,
+        name VARCHAR(256) NOT NULL,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+        updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+        deleted_at TIMESTAMP,
+        PRIMARY KEY(id)
+    ) DEFAULT CHARACTER SET = utf8;
+
+CREATE TABLE
+    IF NOT EXISTS saying (
+        id INT NOT NULL AUTO_INCREMENT,
+        book_id INT NOT NULL,
+        book_saying_id INT NOT NULL,
+        saying VARCHAR(256) NOT NULL,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+        updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+        deleted_at TIMESTAMP,
+        PRIMARY KEY(id)
+    ) DEFAULT CHARACTER SET = utf8;
+
 DROP VIEW IF EXISTS quiz_view;
 
 CREATE VIEW QUIZ_VIEW AS
