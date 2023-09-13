30e5926c5cb175dd431598300d4299dd6e48ea94
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2023-09-13 22:35:54 +0900

    フロント側　API受信・処理用データ型作成

--- frontend-next/src/interfaces/API.ts
+++ frontend-next/src/interfaces/API.ts
@@ -3,3 +3,12 @@ export interface ImageUploadReturnValue {
   isUploading: boolean;
   url: string;
 }
+
+// APIから受け取ったデータを変換しフロント側で処理する型
+export interface ProcessingApiReponse {
+  status: number;
+  body: ApiResponse;
+}
+
+// APIから得られるデータ(抽象クラス)
+export interface ApiResponse {}
