186932b6fe8afb3ac32b790fe04c1aa6ab760c04
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2023-09-14 23:16:00 +0900

    カテゴリ正解率APIの返り値の型をフロントに

--- frontend-next/src/interfaces/API.ts
+++ frontend-next/src/interfaces/API.ts
@@ -22,3 +22,26 @@ export interface QuizFileApiResponse extends ApiResponse {
   updated_at: string;
   deleted_at: string | undefined;
 }
+
+// (指定ファイルのカテゴリ毎の)正解率が入ったDTO(SQL実行結果)
+export interface AccuracyRateByCategorySqlResultDto {
+  file_num: number;
+  c_category: string;
+  count: number;
+  accuracy_rate: number;
+}
+
+// (指定ファイルのチェック済の)正解率が入ったDTO(SQL実行結果)
+export interface AccuracyRateOfCheckedQuizSqlResultDto {
+  checked: boolean;
+  count: number;
+  sum_clear: number;
+  sum_fail: number;
+  accuracy_rate: number;
+}
+
+// カテゴリ正解率取得API 返り値DTO
+export interface GetAccuracyRateByCategoryServiceDto extends ApiResponse {
+  result: AccuracyRateByCategorySqlResultDto[];
+  checked_result: AccuracyRateOfCheckedQuizSqlResultDto[];
+}
