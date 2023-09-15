4baeb3665ebc773143dd73b1ee3035fe5a27c3b0
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2023-09-16 00:39:39 +0900

    quiz_view,categoryの返り値の型

--- frontend-next/src/interfaces/API.ts
+++ frontend-next/src/interfaces/API.ts
@@ -38,9 +38,40 @@ export interface QuizApiResponse extends ApiResponse {
   deleted_at: string | undefined;
 }
 
+// quiz_viewからの取得結果(正解数、不正解数、正解率など)
+export interface QuizViewApiResponse extends ApiResponse {
+  file_num: number;
+  quiz_num: number;
+  quiz_sentense: string;
+  answer: string;
+  category: string | undefined;
+  img_file: string | undefined;
+  checked: boolean | undefined;
+  clear_count: number;
+  fail_count: number;
+  created_at: string;
+  updated_at: string;
+  deleted_at: string | undefined;
+  accuracy_rate: number;
+}
+
+// categoryからの取得結果
+export interface CategoryApiResponse extends ApiResponse {
+  file_num: number;
+  category: string;
+  created_at: string;
+  updated_at: string;
+  deleted_at: string | undefined;
+}
+
 // 問題追加APIの返り値の型
 export interface AddQuizApiResponse extends ApiResponse {
   result: string;
+}
+
+// 問題チェックAPIの返り値の型
+export interface CheckQuizApiResponse extends ApiResponse {
+  result: boolean;
 }
 
 // (指定ファイルのカテゴリ毎の)正解率が入ったDTO(SQL実行結果)
