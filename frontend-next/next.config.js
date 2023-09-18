6581ef3fc6d488ee601e144ab7a80eb20497f9d8
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2023-09-18 18:35:02 +0900

    Expectation Violation: Duplicate atom key  が出ない対応

--- frontend-next/next.config.js
+++ frontend-next/next.config.js
@@ -1,7 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  trailingSlash: true
-};
+// const nextConfig = {
+//   reactStrictMode: true,
+//   trailingSlash: true
+// };
+const withInterceptStdout = require('next-intercept-stdout');
+const nextConfig = withInterceptStdout(
+  {
+    reactStrictMode: true,
+    trailingSlash: true
+  },
+  (text) => (text.includes('Duplicate atom key') ? '' : text)
+);
 
 module.exports = nextConfig;
