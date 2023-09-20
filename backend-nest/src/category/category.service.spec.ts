333f333673f9faed0b4d09bd10d848608ba9d4a1
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2023-09-20 20:55:51 +0900

    カテゴリリストテスト　モック対象をexecQueryにする

--- backend-nest/src/category/category.service.spec.ts
+++ backend-nest/src/category/category.service.spec.ts
@@ -1,5 +1,6 @@
 import { CategoryService } from './category.service';
-jest.mock('./category.service');
+import * as Dao from '../../lib/db/dao';
+jest.mock('../../lib/db/dao');
 
 describe('CategoryService', () => {
   let categoryService: CategoryService;
@@ -18,41 +19,35 @@ describe('CategoryService', () => {
       updated_at: '2000-01-01 00:00:00',
       deleted_at: null,
     };
-    jest
-      .spyOn(categoryService, 'getCategoryList')
-      .mockResolvedValueOnce(testResult);
+    jest.spyOn(Dao, 'execQuery').mockResolvedValueOnce(testResult);
     expect(await categoryService.getCategoryList(0)).toBe(testResult);
   });
 
   // カテゴリリスト取得 異常系
   it('getCategoryList - NG', async () => {
-    const httpError: Error = {
-      message: 'error test by jest.',
-      name: 'error',
-    };
-    jest
-      .spyOn(categoryService, 'getCategoryList')
-      .mockRejectedValueOnce(httpError);
+    jest.spyOn(Dao, 'execQuery').mockImplementation(() => {
+      throw Error('error test by jest.');
+    });
     await expect(categoryService.getCategoryList(0)).rejects.toMatchObject({
       message: 'error test by jest.',
     });
   });
 
-  // カテゴリ総入れ替え 正常系
-  it('replaceAllCategory - OK', async () => {
-    // テストデータ 正常時の返り値
-    const testCategoryResult = [
-      {
-        result: 'OK',
-      },
-    ];
-    jest
-      .spyOn(categoryService, 'replaceAllCategory')
-      .mockResolvedValueOnce(testCategoryResult);
-    expect(
-      await categoryService.replaceAllCategory({
-        file_num: 0,
-      }),
-    ).toBe(testCategoryResult);
-  });
+  // // カテゴリ総入れ替え 正常系
+  // it('replaceAllCategory - OK', async () => {
+  //   // テストデータ 正常時の返り値
+  //   const testCategoryResult = [
+  //     {
+  //       result: 'OK',
+  //     },
+  //   ];
+  //   jest
+  //     .spyOn(categoryService, 'replaceAllCategory')
+  //     .mockResolvedValueOnce(testCategoryResult);
+  //   expect(
+  //     await categoryService.replaceAllCategory({
+  //       file_num: 0,
+  //     }),
+  //   ).toBe(testCategoryResult);
+  // });
 });
