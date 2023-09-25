f946a827b63f75780f0e7c38328733b008f6b9ad
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2023-09-26 01:04:24 +0900

    HeaderBar  module.css効くように対応（cssはmodule.cssにする）

--- frontend-next/src/components/ui-elements/headerBar/HeaderBar.tsx
+++ frontend-next/src/components/ui-elements/headerBar/HeaderBar.tsx
@@ -1,11 +1,13 @@
-import React from 'react';
-
-import './headerBar.css';
+import React, { ReactNode } from 'react';
+import styles from './HeaderBar.module.css';
 
 interface HeaderBarProps {
   bgColor: string;
+  children?: ReactNode;
 }
 
-export const HeaderBar = ({ bgColor = '#0077B6' }: HeaderBarProps) => (
-  <header style={{ backgroundColor: bgColor }}></header>
+export const HeaderBar = ({ bgColor = '#0077B6', ...props }: HeaderBarProps) => (
+  <header className={styles.headerBar} style={{ backgroundColor: bgColor }}>
+    {props.children}
+  </header>
 );
