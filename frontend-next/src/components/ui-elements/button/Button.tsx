e7ee3b59d2b9ba47a8a5a53fa910d1860d9eea87
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2023-09-27 00:43:29 +0900

    Buttonにclass指定追加 CSS操作用

--- frontend-next/src/components/ui-elements/button/Button.tsx
+++ frontend-next/src/components/ui-elements/button/Button.tsx
@@ -4,18 +4,22 @@ import { Button as MuiButton } from '@mui/material';
 
 interface ButtonProps {
   label: string;
+  size?: 'small' | 'medium' | 'large';
   mode?: 'regular' | 'square';
   color?: 'inherit' | 'primary' | 'secondary' | 'success' | 'error' | 'info' | 'warning';
   variant?: 'contained' | 'outlined' | 'text';
   href?: string;
+  class?: string;
   onClick?: () => void;
 }
 
-export const Button = ({ color = 'primary', variant = 'outlined', label, ...props }: ButtonProps) => {
+export const Button = ({ color = 'primary', size = 'medium', variant = 'outlined', label, ...props }: ButtonProps) => {
   return (
     <>
       <MuiButton
-        className={[styles.button, props.mode ? (props.mode === 'regular' ? '' : styles[props.mode]) : ''].join(' ')}
+        className={[styles.button, props.mode ? (props.mode === 'regular' ? '' : styles[props.mode]) : '']
+          .concat(props.class ? props.class.split(' ').map((x) => styles[x] || '') : [])
+          .join(' ')}
         variant={variant}
         size={'medium'}
         color={color}
