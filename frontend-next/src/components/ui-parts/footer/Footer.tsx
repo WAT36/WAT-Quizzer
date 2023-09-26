6b57711a84eb5870e0c7103b583037c0deaa0cd3
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2023-09-27 00:44:08 +0900

    Footerコンポーネント 追加　storybookで表示できない？

--- frontend-next/src/components/ui-parts/footer/Footer.tsx
+++ frontend-next/src/components/ui-parts/footer/Footer.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 
-import './footer.css';
-import { Button } from '@mui/material';
+import styles from './Footer.module.css';
+import { FooterBar } from '@/components/ui-elements/footerBar/FooterBar';
+import { Button } from '@/components/ui-elements/button/Button';
 
 interface FooterProps {
   bgColor: string;
@@ -9,12 +10,10 @@ interface FooterProps {
 }
 
 export const Footer = ({ bgColor = '#0077B6', topHref }: FooterProps) => (
-  <footer style={{ backgroundColor: bgColor }}>
-    <span className="left">
-      <Button size="small" color="inherit" href={topHref}>
-        トップ
-      </Button>
+  <FooterBar bgColor={bgColor}>
+    <span className={styles.left}>
+      <Button class="no-margin no-border" size="small" color="inherit" href={topHref} label="トップ" />
     </span>
-    <span className="right">©️ Tatsuroh Wakasugi</span>
-  </footer>
+    <span className={styles.right}>©️ Tatsuroh Wakasugi</span>
+  </FooterBar>
 );
