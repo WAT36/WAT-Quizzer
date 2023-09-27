b77d7f5b8a73b82df4b2c925cff6cc61fefcf8cf
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2023-09-28 00:36:16 +0900

    ページレイアウト　のStoryを追加

---
+++ frontend-next/src/components/templates/layout/Layout.tsx
@@ -0,0 +1,96 @@
+import React from 'react';
+
+import styles from './Layout.module.css';
+import { List, ListItem } from '@mui/material';
+import Head from 'next/head';
+import { Header } from '@/components/ui-parts/header/Header';
+import { SideBar, toggleDrawer } from '@/components/ui-parts/sideBar/SideBar';
+import { Footer } from '@/components/ui-parts/footer/Footer';
+import { useRecoilState } from 'recoil';
+import { isOpenState } from '@/atoms/SideBar';
+import { Button } from '@/components/ui-elements/button/Button';
+
+interface LayoutConfig {
+  bgColor: string;
+  sideBarContents: {
+    name: string;
+    link: string;
+  }[];
+}
+
+interface LayoutProps {
+  title?: string;
+  contents: JSX.Element;
+  mode: 'quizzer' | 'englishBot';
+}
+
+const urlEnd = process.env.NEXT_PUBLIC_URL_END || '';
+const config: { [key: string]: LayoutConfig } = {
+  quizzer: {
+    bgColor: '#0077B6',
+    sideBarContents: [
+      { name: 'トップ', link: urlEnd },
+      { name: '問題出題', link: '/quizzer' + urlEnd },
+      { name: '問題追加', link: '/quizzer/addQuiz' + urlEnd },
+      { name: '問題編集', link: '/quizzer/editQuiz' + urlEnd },
+      { name: '問題検索', link: '/quizzer/searchQuiz' + urlEnd },
+      { name: '問題削除', link: '/quizzer/deleteQuiz' + urlEnd },
+      { name: 'カテゴリ別正解率表示', link: '/quizzer/accuracyRateGraph' + urlEnd },
+      { name: '画像アップロード', link: '/quizzer/imageUpload' + urlEnd },
+      { name: '設定', link: '/quizzer/settings' + urlEnd }
+    ]
+  },
+  englishBot: {
+    bgColor: 'midnightblue',
+    sideBarContents: [
+      { name: 'Top', link: '/englishBot' + urlEnd },
+      { name: 'Add Words', link: '/englishBot/addWord' + urlEnd },
+      { name: 'Dictionary', link: '/englishBot/dictionary' + urlEnd },
+      { name: 'Detail Word', link: '/englishBot/detailWord' + urlEnd },
+      { name: 'Add Example', link: '/englishBot/addExample' + urlEnd }
+    ]
+  }
+};
+
+export const Layout = ({ title, contents, mode = 'quizzer' }: LayoutProps) => {
+  const [sidebarState, setSidebarState] = useRecoilState(isOpenState);
+  const modeConfig = config[mode];
+  return (
+    <>
+      {/*Head タイトルなど*/}
+      <Head>
+        <title>{'WAT Quizzer ' + title}</title>
+      </Head>
+      {/*ヘッダ*/}
+      <Header bgColor={modeConfig.bgColor} onClick={toggleDrawer(true, setSidebarState)}></Header>
+
+      {/*サイドバー*/}
+      <SideBar>
+        <List>
+          {modeConfig.sideBarContents.map((value) => (
+            <ListItem key={value.name}>
+              <Button
+                variant="text"
+                href={value.link}
+                label={value.name}
+                onClick={toggleDrawer(false, setSidebarState)}
+              />
+            </ListItem>
+          ))}
+        </List>
+      </SideBar>
+
+      {/*ヘッダとコンテンツ間の調整余白 */}
+      <div className={styles.space}></div>
+
+      {/*内容*/}
+      {contents}
+
+      {/*フッタとコンテンツ間の調整余白 */}
+      <div className={styles.space}></div>
+
+      {/*フッタ*/}
+      <Footer bgColor={modeConfig.bgColor} topHref={process.env.NEXT_PUBLIC_URL_END || ''}></Footer>
+    </>
+  );
+};
