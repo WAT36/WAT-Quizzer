2a5cdf36216ff600acdd49585bf6544427c43cdd
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2023-09-30 23:50:14 +0900

    応用題追加機能　問題追加画面で実装

--- frontend-next/src/pages/quizzer/addQuiz.tsx
+++ frontend-next/src/pages/quizzer/addQuiz.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { memo, useEffect, useState } from 'react';
 
 import { get, post } from '../../common/API';
 import { buttonStyle, messageBoxStyle, typoStyles } from '../../styles/Pages';
@@ -14,12 +14,36 @@ import {
   MenuItem,
   Select,
   SelectChangeEvent,
+  Tab,
+  Tabs,
   Typography
 } from '@mui/material';
 import { addQuizDto } from '../../../interfaces/api/response';
 import { AddQuizApiResponse, ProcessingApiReponse } from '../../../interfaces/api/response';
 import { QuizFileApiResponse } from '../../../interfaces/db';
 import { Layout } from '@/components/templates/layout/Layout';
+
+export interface TabPanelProps {
+  children?: React.ReactNode;
+  index: number;
+  value: number;
+}
+
+// タブ内コンテンツの中身、別コンポーネント化
+// eslint-disable-next-line react/display-name
+const CustomTabPanel = memo<TabPanelProps>((props: TabPanelProps) => {
+  const { children, value, index } = props;
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+    >
+      {value === index && <>{children}</>}
+    </div>
+  );
+});
 
 export default function AddQuizPage() {
   const [file_num, setFileNum] = useState<number>(-1);
@@ -28,6 +52,8 @@ export default function AddQuizPage() {
   const [messageColor, setMessageColor] = useState<string>('common.black');
   const [addLog, setAddLog] = useState<string>();
   const [filelistoption, setFilelistoption] = useState<JSX.Element[]>();
+
+  const [value, setValue] = React.useState(0);
 
   useEffect(() => {
     setMessage('通信中...');
@@ -70,30 +96,59 @@ export default function AddQuizPage() {
 
     setMessage('通信中...');
     setMessageColor('#d3d3d3');
-    post(
-      '/quiz/add',
-      {
-        file_num,
-        input_data
-      },
-      (data: ProcessingApiReponse) => {
-        if (data.status === 200 || data.status === 201) {
-          const res: AddQuizApiResponse[] = data.body as AddQuizApiResponse[];
-          setMessage('Success!! 問題を追加できました!');
-          setMessageColor('success.light');
-          setAddLog(res[0].result);
-          setInputData({});
-          //入力データをクリア
-          const inputQuizField = document.getElementsByTagName('textarea').item(0) as HTMLTextAreaElement;
-          if (inputQuizField) {
-            inputQuizField.value = '';
+    // 基礎問題のとき(value==0)
+    if (value === 0) {
+      post(
+        '/quiz/add',
+        {
+          file_num,
+          input_data
+        },
+        (data: ProcessingApiReponse) => {
+          if (data.status === 200 || data.status === 201) {
+            const res: AddQuizApiResponse[] = data.body as AddQuizApiResponse[];
+            setMessage('Success!! 問題を追加できました!');
+            setMessageColor('success.light');
+            setAddLog(res[0].result);
+            setInputData({});
+            //入力データをクリア
+            const inputQuizField = document.getElementsByTagName('textarea').item(0) as HTMLTextAreaElement;
+            if (inputQuizField) {
+              inputQuizField.value = '';
+            }
+          } else {
+            setMessage('エラー:外部APIとの連携に失敗しました');
+            setMessageColor('error');
           }
-        } else {
-          setMessage('エラー:外部APIとの連携に失敗しました');
-          setMessageColor('error');
         }
-      }
-    );
+      );
+    } else if (value === 1) {
+      // 応用問題の時(value===1)
+      post(
+        '/quiz/advanced',
+        {
+          file_num,
+          input_data
+        },
+        (data: ProcessingApiReponse) => {
+          if (data.status === 200 || data.status === 201) {
+            const res: AddQuizApiResponse[] = data.body as AddQuizApiResponse[];
+            setMessage('Success!! 問題を追加できました!');
+            setMessageColor('success.light');
+            setAddLog(res[0].result);
+            setInputData({});
+            //入力データをクリア
+            const inputQuizField = document.getElementsByTagName('textarea').item(0) as HTMLTextAreaElement;
+            if (inputQuizField) {
+              inputQuizField.value = '';
+            }
+          } else {
+            setMessage('エラー:外部APIとの連携に失敗しました');
+            setMessageColor('error');
+          }
+        }
+      );
+    }
   };
 
   // 入力データを登録
@@ -102,6 +157,20 @@ export default function AddQuizPage() {
       ...prev,
       [attrName]: value
     }));
+  };
+
+  // タブ内コンテンツの設定
+  const a11yProps = (index: number) => {
+    return {
+      id: `simple-tab-${index}`,
+      'aria-controls': `simple-tabpanel-${index}`
+    };
+  };
+
+  // タブの切り替え
+  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+    setValue(newValue);
+    setInputData({});
   };
 
   const contents = () => {
@@ -135,51 +204,104 @@ export default function AddQuizPage() {
           </FormControl>
 
           <Card variant="outlined">
-            <CardContent>
-              <Typography variant="h6" component="h6" style={messageBoxStyle}>
-                追加する問題（問題文,正解,カテゴリ,画像ファイル名）
-              </Typography>
+            <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
+              <Tab label="基礎問題" {...a11yProps(0)} />
+              <Tab label="応用問題" {...a11yProps(1)} />
+            </Tabs>
+            <CustomTabPanel value={value} index={0}>
+              <CardContent>
+                <Typography variant="h6" component="h6" style={messageBoxStyle}>
+                  追加する基礎問題（問題文,正解,カテゴリ,画像ファイル名）
+                </Typography>
 
-              <Typography variant="h6" component="h6" style={messageBoxStyle}>
-                問題文　：
-                <Input
-                  fullWidth
-                  maxRows={1}
-                  value={input_data.question || ''}
-                  onChange={(e) => updateInputData('question', e.target.value)}
-                />
-              </Typography>
+                <Typography variant="h6" component="h6" style={messageBoxStyle}>
+                  問題文　：
+                  <Input
+                    fullWidth
+                    maxRows={1}
+                    value={input_data.question || ''}
+                    onChange={(e) => updateInputData('question', e.target.value)}
+                  />
+                </Typography>
 
-              <Typography variant="h6" component="h6" style={messageBoxStyle}>
-                答え　　：
-                <Input
-                  fullWidth
-                  maxRows={1}
-                  value={input_data.answer || ''}
-                  onChange={(e) => updateInputData('answer', e.target.value)}
-                />
-              </Typography>
+                <Typography variant="h6" component="h6" style={messageBoxStyle}>
+                  答え　　：
+                  <Input
+                    fullWidth
+                    maxRows={1}
+                    value={input_data.answer || ''}
+                    onChange={(e) => updateInputData('answer', e.target.value)}
+                  />
+                </Typography>
 
-              <Typography variant="h6" component="h6" style={messageBoxStyle}>
-                カテゴリ：
-                <Input
-                  fullWidth
-                  maxRows={1}
-                  value={input_data.category || ''}
-                  onChange={(e) => updateInputData('category', e.target.value)}
-                />
-              </Typography>
+                <Typography variant="h6" component="h6" style={messageBoxStyle}>
+                  カテゴリ：
+                  <Input
+                    fullWidth
+                    maxRows={1}
+                    value={input_data.category || ''}
+                    onChange={(e) => updateInputData('category', e.target.value)}
+                  />
+                </Typography>
 
-              <Typography variant="h6" component="h6" style={messageBoxStyle}>
-                画像ファイル名：
-                <Input
-                  fullWidth
-                  maxRows={1}
-                  value={input_data.img_file || ''}
-                  onChange={(e) => updateInputData('img_data', e.target.value)}
-                />
-              </Typography>
-            </CardContent>
+                <Typography variant="h6" component="h6" style={messageBoxStyle}>
+                  画像ファイル名：
+                  <Input
+                    fullWidth
+                    maxRows={1}
+                    value={input_data.img_file || ''}
+                    onChange={(e) => updateInputData('img_data', e.target.value)}
+                  />
+                </Typography>
+              </CardContent>
+            </CustomTabPanel>
+            <CustomTabPanel value={value} index={1}>
+              <CardContent>
+                <Typography variant="h6" component="h6" style={messageBoxStyle}>
+                  追加する応用問題（問題文,正解,カテゴリ,画像ファイル名）
+                </Typography>
+
+                <Typography variant="h6" component="h6" style={messageBoxStyle}>
+                  応用問題文　：
+                  <Input
+                    fullWidth
+                    maxRows={1}
+                    value={input_data.question || ''}
+                    onChange={(e) => updateInputData('question', e.target.value)}
+                  />
+                </Typography>
+
+                <Typography variant="h6" component="h6" style={messageBoxStyle}>
+                  応用問題の答え　　：
+                  <Input
+                    fullWidth
+                    maxRows={1}
+                    value={input_data.answer || ''}
+                    onChange={(e) => updateInputData('answer', e.target.value)}
+                  />
+                </Typography>
+
+                <Typography variant="h6" component="h6" style={{ ...messageBoxStyle, visibility: 'hidden' }}>
+                  カテゴリ：
+                  <Input
+                    fullWidth
+                    maxRows={1}
+                    value={input_data.category || ''}
+                    onChange={(e) => updateInputData('category', e.target.value)}
+                  />
+                </Typography>
+
+                <Typography variant="h6" component="h6" style={messageBoxStyle}>
+                  画像ファイル名：
+                  <Input
+                    fullWidth
+                    maxRows={1}
+                    value={input_data.img_file || ''}
+                    onChange={(e) => updateInputData('img_data', e.target.value)}
+                  />
+                </Typography>
+              </CardContent>
+            </CustomTabPanel>
           </Card>
         </FormGroup>
 
