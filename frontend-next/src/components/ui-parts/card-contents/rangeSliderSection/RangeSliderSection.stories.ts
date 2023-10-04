837e3e3a0a7d6b314c6a1dd2f30c309559270a40
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2023-10-04 22:42:48 +0900

    RangeSliderとタイトルを一緒にしたRangeSliderSection　をstoryに追加

---
+++ frontend-next/src/components/ui-parts/card-contents/rangeSliderSection/RangeSliderSection.stories.ts
@@ -0,0 +1,22 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { RangeSliderSection } from './RangeSliderSection';
+
+const meta = {
+  title: 'Organisms/CardContents/RangeSliderSection',
+  component: RangeSliderSection,
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
+  tags: ['autodocs'],
+  parameters: {
+    // More on how to position stories at: https://storybook.js.org/docs/react/configure/story-layout
+    layout: 'padded'
+  }
+} satisfies Meta<typeof RangeSliderSection>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const rangeSliderSection: Story = {
+  args: {
+    sectionTitle: '(タイトル)'
+  }
+};
