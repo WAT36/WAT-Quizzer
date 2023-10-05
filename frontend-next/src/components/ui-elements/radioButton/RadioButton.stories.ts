6cdf4a64e42e8c67403c2cf4b974cea1f7a0df34
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2023-10-05 22:29:20 +0900

    ラジオボタン （単体）のstory作成

---
+++ frontend-next/src/components/ui-elements/radioButton/RadioButton.stories.ts
@@ -0,0 +1,23 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { RadioButton } from './RadioButton';
+
+const meta = {
+  title: 'Atom/RadioButton',
+  component: RadioButton,
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
+  tags: ['autodocs'],
+  parameters: {
+    // More on how to position stories at: https://storybook.js.org/docs/react/configure/story-layout
+    layout: 'padded'
+  }
+} satisfies Meta<typeof RadioButton>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const radioButton: Story = {
+  args: {
+    value: 'value',
+    label: 'ラベル'
+  }
+};
