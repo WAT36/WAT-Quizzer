c1f41ae9a028e20a8d6b079f610fbcdf4572784b
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2023-10-05 23:53:54 +0900

    MessageCard stories  フォルダ位置変更

--- frontend-next/src/components/ui-parts/messageCard/MessageCard.stories.ts
+++ frontend-next/src/components/ui-parts/messageCard/MessageCard.stories.ts
@@ -2,7 +2,7 @@ import type { Meta, StoryObj } from '@storybook/react';
 import { MessageCard } from './MessageCard';
 
 const meta = {
-  title: 'Molecules/CardContents/MessageCard',
+  title: 'Molecules/MessageCard',
   component: MessageCard,
   // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
   tags: ['autodocs'],
