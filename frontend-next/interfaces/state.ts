d3f939265556862aeff7ac91b2104c53cd9aa43a
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2023-10-09 07:00:21 +0900

    画面メッセージステートと表示する問題のステートの型追加　こっちを使おう

---
+++ frontend-next/interfaces/state.ts
@@ -0,0 +1,11 @@
+export interface MessageState {
+  message: string;
+  messageColor: string;
+}
+
+export interface DisplayQuizState {
+  quizSentense: string;
+  quizAnswer: string;
+  checked: boolean;
+  expanded: boolean;
+}
