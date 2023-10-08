3db9f837620aaa1cf0be4fd4329737c72a4129ac
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2023-10-09 07:00:37 +0900

    問題出題ボタンのstory追加

---
+++ frontend-next/src/components/ui-parts/button-patterns/getQuiz/GetQuiz.button.tsx
@@ -0,0 +1,100 @@
+import React from 'react';
+import { Button } from '@/components/ui-elements/button/Button';
+import { ProcessingApiReponse } from '../../../../../interfaces/api/response';
+import { QuizApiResponse } from '../../../../../interfaces/db';
+import { get } from '@/common/API';
+import { DisplayQuizState, MessageState } from '../../../../../interfaces/state';
+
+interface GetQuizButtonProps {
+  label: string;
+  file_num: number;
+  quiz_num: number;
+  format: string;
+  setMessageStater?: React.Dispatch<React.SetStateAction<MessageState>>;
+  setDisplayQuizStater?: React.Dispatch<React.SetStateAction<DisplayQuizState>>;
+}
+
+const getQuizAPI = ({
+  label,
+  file_num,
+  quiz_num,
+  format,
+  setMessageStater,
+  setDisplayQuizStater
+}: GetQuizButtonProps) => {
+  // 設定ステートない場合はreturn(storybook表示用に設定)
+  if (!setMessageStater || !setDisplayQuizStater) {
+    return;
+  }
+  if (file_num === -1) {
+    setMessageStater({
+      message: 'エラー:問題ファイルを選択して下さい',
+      messageColor: 'error'
+    });
+    return;
+  } else if (!quiz_num) {
+    setMessageStater({
+      message: 'エラー:問題番号を入力して下さい',
+      messageColor: 'error'
+    });
+    return;
+  }
+
+  setMessageStater({
+    message: '通信中...',
+    messageColor: '#d3d3d3'
+  });
+  get(
+    '/quiz',
+    (data: ProcessingApiReponse) => {
+      if (data.status === 404 || data.body?.length === 0) {
+        setMessageStater({
+          message: 'エラー:条件に合致するデータはありません',
+          messageColor: 'error'
+        });
+      } else if (data.status === 200) {
+        const res: QuizApiResponse[] = data.body as QuizApiResponse[];
+        setDisplayQuizStater({
+          quizSentense: '[' + res[0].file_num + '-' + res[0].quiz_num + ']' + res[0].quiz_sentense,
+          quizAnswer: res[0].answer,
+          checked: res[0].checked || false,
+          expanded: false
+        });
+        setMessageStater({
+          message: '　',
+          messageColor: 'success.light'
+        });
+      } else {
+        setMessageStater({
+          message: 'エラー:外部APIとの連携に失敗しました',
+          messageColor: 'error'
+        });
+      }
+    },
+    {
+      file_num: String(file_num),
+      quiz_num: String(quiz_num),
+      format
+    }
+  );
+};
+
+export const GetQuizButton = ({
+  label = '問題出題',
+  file_num,
+  quiz_num,
+  format,
+  setMessageStater,
+  setDisplayQuizStater
+}: GetQuizButtonProps) => {
+  return (
+    <>
+      <Button
+        label={label}
+        variant="contained"
+        color="primary"
+        onClick={(e) => getQuizAPI({ label, file_num, quiz_num, format, setMessageStater, setDisplayQuizStater })}
+      />
+    </>
+  );
+};
