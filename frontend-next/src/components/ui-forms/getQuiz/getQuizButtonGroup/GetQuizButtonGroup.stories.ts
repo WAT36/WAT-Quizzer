39db7a7ed38cb5bb4d05151cce8fa92b70e51137
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2023-10-14 09:44:47 +0900

    問題出題ページの問題出題系ボタングループstory追加

--- frontend-next/src/components/ui-forms/getQuiz/getQuizButtonGroup/GetQuizButtonGroup.stories.ts
+++ frontend-next/src/components/ui-forms/getQuiz/getQuizButtonGroup/GetQuizButtonGroup.stories.ts
@@ -1,24 +1,24 @@
 import type { Meta, StoryObj } from '@storybook/react';
 
-import { GetQuizButton } from './GetQuiz.button';
+import { GetQuizButtonGroup } from './GetQuizButtonGroup';
 
 // More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 const meta = {
-  title: 'Molecules/ButtonPatterns/GetQuiz',
-  component: GetQuizButton,
+  title: 'Organisms/GetQuiz/GetQuizButtonGroup',
+  component: GetQuizButtonGroup,
   parameters: {
     // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/react/configure/story-layout
     layout: 'centered'
   },
   // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
   tags: ['autodocs']
-} satisfies Meta<typeof GetQuizButton>;
+} satisfies Meta<typeof GetQuizButtonGroup>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
 // More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
-export const GetQuiz: Story = {
+export const GetQuizButtons: Story = {
   args: {
     queryOfQuizState: {
       fileNum: 0,
