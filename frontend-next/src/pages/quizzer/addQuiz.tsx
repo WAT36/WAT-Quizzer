6696c9495e37173f2829acbcdb2afdf5f4c37c10
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2023-10-20 21:12:42 +0900

    問題追加画面にAddQuizButton適用

--- frontend-next/src/pages/quizzer/addQuiz.tsx
+++ frontend-next/src/pages/quizzer/addQuiz.tsx
@@ -19,7 +19,7 @@ import { addQuizDto } from '../../../interfaces/api/response';
 import { AddQuizApiResponse, ProcessingApiReponse } from '../../../interfaces/api/response';
 import { QuizFileApiResponse } from '../../../interfaces/db';
 import { Layout } from '@/components/templates/layout/Layout';
-import { MessageState, PullDownOptionState } from '../../../interfaces/state';
+import { MessageState, PullDownOptionState, QueryOfAddQuizState } from '../../../interfaces/state';
 import { AddQuizLogSection } from '@/components/ui-forms/quizzer/addQuiz/addQuizLogSection/AddQuizLogSection';
 import { MessageCard } from '@/components/ui-parts/messageCard/MessageCard';
 import { Title } from '@/components/ui-elements/title/Title';
@@ -28,10 +28,12 @@ import { AppliedTabPanel } from '@/components/ui-parts/tabpanel-patterns/addQuiz
 import { FourChoiceTabPanel } from '@/components/ui-parts/tabpanel-patterns/addQuiz/fourChoiceTabPanel/FourChoice.tabpanel';
 import { PullDown } from '@/components/ui-elements/pullDown/PullDown';
 import { getFileList } from '@/common/response';
+import { AddQuizButton } from '@/components/ui-parts/button-patterns/addQuiz/AddQuiz.button';
 
 export default function AddQuizPage() {
-  const [file_num, setFileNum] = useState<number>(-1);
-  const [input_data, setInputData] = useState<addQuizDto>({});
+  const [queryOfAddQuiz, setQueryOfAddQuiz] = useState<QueryOfAddQuizState>({
+    fileNum: -1
+  });
   const [message, setMessage] = useState<MessageState>({ message: '　', messageColor: 'common.black' });
   const [addLog, setAddLog] = useState<string>('');
   const [filelistoption, setFilelistoption] = useState<PullDownOptionState[]>([]);
@@ -44,119 +46,10 @@ export default function AddQuizPage() {
   }, []);
 
   const selectedFileChange = (e: SelectChangeEvent<number>) => {
-    setFileNum(e.target.value as number);
-  };
-
-  const addQuiz = () => {
-    if (file_num === -1) {
-      setMessage({
-        message: 'エラー:問題ファイルを選択して下さい',
-        messageColor: 'error'
-      });
-      return;
-    } else if (!input_data.question || !input_data.answer) {
-      setMessage({
-        message: 'エラー:追加する問題文・正解を入力して下さい',
-        messageColor: 'error'
-      });
-      return;
-    }
-
-    setMessage({
-      message: '通信中...',
-      messageColor: '#d3d3d3'
-    });
-    // 基礎問題のとき(value==0)
-    if (value === 0) {
-      post(
-        '/quiz/add',
-        {
-          file_num,
-          input_data
-        },
-        (data: ProcessingApiReponse) => {
-          if (data.status === 200 || data.status === 201) {
-            const res: AddQuizApiResponse[] = data.body as AddQuizApiResponse[];
-            setMessage({
-              message: 'Success!! 問題を追加できました!',
-              messageColor: 'success.light'
-            });
-            setAddLog(res[0].result);
-            setInputData({});
-            //入力データをクリア
-            const inputQuizField = document.getElementsByTagName('textarea').item(0) as HTMLTextAreaElement;
-            if (inputQuizField) {
-              inputQuizField.value = '';
-            }
-          } else {
-            setMessage({
-              message: 'エラー:外部APIとの連携に失敗しました',
-              messageColor: 'error'
-            });
-          }
-        }
-      );
-    } else if (value === 1) {
-      // 応用問題の時(value===1)
-      post(
-        '/quiz/advanced',
-        {
-          file_num,
-          input_data
-        },
-        (data: ProcessingApiReponse) => {
-          if (data.status === 200 || data.status === 201) {
-            const res: AddQuizApiResponse[] = data.body as AddQuizApiResponse[];
-            setMessage({
-              message: 'Success!! 問題を追加できました!',
-              messageColor: 'success.light'
-            });
-            setAddLog(res[0].result);
-            setInputData({});
-            //入力データをクリア
-            const inputQuizField = document.getElementsByTagName('textarea').item(0) as HTMLTextAreaElement;
-            if (inputQuizField) {
-              inputQuizField.value = '';
-            }
-          } else {
-            setMessage({
-              message: 'エラー:外部APIとの連携に失敗しました',
-              messageColor: 'error'
-            });
-          }
-        }
-      );
-    } else if (value === 2) {
-      // 四択問題の時(value===2)
-      post(
-        '/quiz/advanced/4choice',
-        {
-          file_num,
-          input_data
-        },
-        (data: ProcessingApiReponse) => {
-          if (data.status === 200 || data.status === 201) {
-            const res: AddQuizApiResponse[] = data.body as AddQuizApiResponse[];
-            setMessage({
-              message: 'Success!! 問題を追加できました!',
-              messageColor: 'success.light'
-            });
-            setAddLog(res[0].result);
-            setInputData({});
-            //入力データをクリア
-            const inputQuizField = document.getElementsByTagName('textarea').item(0) as HTMLTextAreaElement;
-            if (inputQuizField) {
-              inputQuizField.value = '';
-            }
-          } else {
-            setMessage({
-              message: 'エラー:外部APIとの連携に失敗しました',
-              messageColor: 'error'
-            });
-          }
-        }
-      );
-    }
+    setQueryOfAddQuiz((prev) => ({
+      ...prev,
+      ['fileNum']: e.target.value as number
+    }));
   };
 
   // タブ内コンテンツの設定
@@ -170,7 +63,9 @@ export default function AddQuizPage() {
   // タブの切り替え
   const handleChange = (event: React.SyntheticEvent, newValue: number) => {
     setValue(newValue);
-    setInputData({});
+    setQueryOfAddQuiz((prev) => ({
+      fileNum: queryOfAddQuiz.fileNum
+    }));
   };
 
   const contents = () => {
@@ -191,15 +86,34 @@ export default function AddQuizPage() {
               <Tab label="応用問題" {...a11yProps(1)} />
               <Tab label="四択問題" {...a11yProps(2)} />
             </Tabs>
-            <BasisTabPanel value={value} index={0} inputData={input_data} setInputData={setInputData} />
-            <AppliedTabPanel value={value} index={1} inputData={input_data} setInputData={setInputData} />
-            <FourChoiceTabPanel value={value} index={2} inputData={input_data} setInputData={setInputData} />
+            <BasisTabPanel
+              value={value}
+              index={0}
+              queryOfAddQuizState={queryOfAddQuiz}
+              setQueryofAddQuizStater={setQueryOfAddQuiz}
+            />
+            <AppliedTabPanel
+              value={value}
+              index={1}
+              queryOfAddQuizState={queryOfAddQuiz}
+              setQueryofAddQuizStater={setQueryOfAddQuiz}
+            />
+            <FourChoiceTabPanel
+              value={value}
+              index={2}
+              queryOfAddQuizState={queryOfAddQuiz}
+              setQueryofAddQuizStater={setQueryOfAddQuiz}
+            />
           </Card>
         </FormGroup>
 
-        <Button style={buttonStyle} variant="contained" color="primary" onClick={(e) => addQuiz()}>
-          送信
-        </Button>
+        <AddQuizButton
+          value={value}
+          queryOfAddQuizState={queryOfAddQuiz}
+          setAddLog={setAddLog}
+          setMessageStater={setMessage}
+          setQueryofAddQuizStater={setQueryOfAddQuiz}
+        />
 
         <AddQuizLogSection log={addLog} />
       </Container>
