75ec4956930bffbda150d79dda04ec36fca781b2
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2023-10-21 21:31:45 +0900

    トップページのボタングループ story化

---
+++ frontend-next/src/components/ui-forms/top/buttonGroup/TopButtonGroup.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Button } from '@/components/ui-elements/button/Button';
+
+export const TopButtonGroup = () => {
+  return (
+    <div>
+      <Button
+        label={'Quizzer'}
+        attr={'top-button'}
+        variant="contained"
+        size="large"
+        color="primary"
+        href={'/quizzer' + process.env.NEXT_PUBLIC_URL_END}
+      />
+      <Button
+        label={'English Quiz Bot'}
+        attr={'top-button'}
+        variant="contained"
+        size="large"
+        color="secondary"
+        href={'/englishBot' + process.env.NEXT_PUBLIC_URL_END}
+      />
+      <Button
+        label={'設定'}
+        attr={'top-button'}
+        variant="contained"
+        size="large"
+        color="info"
+        href={'/settings' + process.env.NEXT_PUBLIC_URL_END}
+      />
+    </div>
+  );
+};
