48d3dae8d6f4b1026f94be9952023896c99e4732
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2023-10-29 00:02:57 +0900

    出典リスト取得関数作成

--- frontend-next/src/common/response.ts
+++ frontend-next/src/common/response.ts
@@ -97,3 +97,40 @@ export const getBook = (
     }
   });
 };
+
+// englishbot用 出典リストをapi通信して取ってくる
+export const getSourceList = (
+  setMessageStater: React.Dispatch<React.SetStateAction<MessageState>>,
+  setSourcelistoption: React.Dispatch<React.SetStateAction<PullDownOptionState[]>>
+) => {
+  setMessageStater({ message: '通信中...', messageColor: '#d3d3d3' });
+
+  const storageKey = 'sourceList';
+  const savedFileList = sessionStorage.getItem(storageKey);
+  if (!savedFileList) {
+    get('/english/source', (data: ProcessingApiReponse) => {
+      if (data.status === 200) {
+        const result: GetSelfHelpBookResponse[] = data.body as GetSelfHelpBookResponse[];
+        let sourcelist: PullDownOptionState[] = [];
+        for (var i = 0; i < result.length; i++) {
+          sourcelist.push({
+            value: String(result[i].id),
+            label: result[i].name
+          });
+        }
+        setSourcelistoption(sourcelist);
+        console.log(JSON.stringify(sourcelist));
+        setMessageStater({ message: '　', messageColor: 'common.black' });
+      } else {
+        setMessageStater({ message: 'エラー:外部APIとの連携に失敗しました', messageColor: 'error' });
+      }
+    });
+  } else {
+    // 既にsession storageに値が入っている場合はそれを利用する
+    setSourcelistoption(JSON.parse(savedFileList));
+    setMessageStater({
+      message: '　',
+      messageColor: 'common.black'
+    });
+  }
+};
