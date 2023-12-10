fcefc73784d513428896dabf70c4d15d94425ab5
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2023-12-10 19:39:46 +0900

    四択ランダム取得API  解説取ってなかったので追加

--- backend-nest/config/sql.ts
+++ backend-nest/config/sql.ts
@@ -447,7 +447,8 @@ export const SQL = {
             a.answer,
             a.img_file,
             a.checked,
-            d.dummy_choice_sentense
+            d.dummy_choice_sentense,
+            e.explanation
           FROM 
           ( SELECT * FROM 
             advanced_quiz_view 
@@ -463,6 +464,10 @@ export const SQL = {
             dummy_choice as d
           ON
             a.id = d.advanced_quiz_id
+          LEFT OUTER JOIN
+            advanced_quiz_explanation as e 
+          ON
+            a.id = e.advanced_quiz_id
         `,
       },
       WORST: {
