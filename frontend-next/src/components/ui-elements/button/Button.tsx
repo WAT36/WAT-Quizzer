63947fa697f57525a11307ad52fce994f71341b2
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2023-12-13 23:50:14 +0900

    Button story クラス設定変更

--- frontend-next/src/components/ui-elements/button/Button.tsx
+++ frontend-next/src/components/ui-elements/button/Button.tsx
@@ -5,7 +5,6 @@ import { Button as MuiButton } from '@mui/material';
 interface ButtonProps {
   label: string;
   size?: 'small' | 'medium' | 'large';
-  mode?: 'regular' | 'square';
   color?: 'inherit' | 'primary' | 'secondary' | 'success' | 'error' | 'info' | 'warning';
   variant?: 'contained' | 'outlined' | 'text';
   href?: string;
@@ -18,11 +17,9 @@ export const Button = ({ color = 'primary', size = 'medium', variant = 'outlined
   return (
     <>
       <MuiButton
-        className={[styles.button, props.mode ? (props.mode === 'regular' ? '' : styles[props.mode]) : '']
-          .concat(props.attr ? props.attr.split(' ').map((x) => styles[x] || '') : [])
-          .join(' ')}
+        className={(props.attr ? props.attr.split(' ').map((x) => styles[x] || '') : []).join(' ')}
         variant={variant}
-        size={'medium'}
+        size={size}
         color={color}
         {...props}
       >
