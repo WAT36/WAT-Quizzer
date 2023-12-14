36b654dd35330b096152ed2eb7c9756f04f20fc9
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2023-12-14 21:53:47 +0900

    ClearQuizButton削除

--- frontend-next/src/components/ui-forms/quizzer/getQuiz/displayQuizSection/DisplayQuizSection.tsx
+++ frontend-next/src/components/ui-forms/quizzer/getQuiz/displayQuizSection/DisplayQuizSection.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { DisplayQuizState, MessageState, QueryOfQuizState } from '../../../../../../interfaces/state';
-import { Button, CardActions, CardContent, Collapse, Typography } from '@mui/material';
-import { ClearQuizButton } from '@/components/ui-parts/button-patterns/clearQuiz/ClearQuiz.button';
+import { Button as MuiButton, CardActions, CardContent, Collapse, Typography } from '@mui/material';
 import { FailQuizButton } from '@/components/ui-parts/button-patterns/failQuiz/FailQuiz.button';
 import { ReverseCheckQuizButton } from '@/components/ui-parts/button-patterns/reverseCheckQuiz/reverseCheckQuiz.button';
 import { Card } from '@/components/ui-elements/card/Card';
+import { clearQuizAPI } from '@/common/ButtonAPI';
+import { Button } from '@/components/ui-elements/button/Button';
 
 interface DisplayQuizSectionProps {
   queryOfQuizState: QueryOfQuizState;
@@ -44,9 +45,9 @@ export const DisplayQuizSection = ({
         </CardContent>
 
         <CardActions>
-          <Button size="small" onClick={handleExpandClick} aria-expanded={displayQuizState.expanded}>
+          <MuiButton size="small" onClick={handleExpandClick} aria-expanded={displayQuizState.expanded}>
             答え
-          </Button>
+          </MuiButton>
         </CardActions>
         <Collapse in={displayQuizState.expanded} timeout="auto" unmountOnExit>
           <CardContent>
@@ -56,11 +57,19 @@ export const DisplayQuizSection = ({
             <Typography variant="subtitle2" component="h4">
               {'解説：' + displayQuizState.explanation}
             </Typography>
-            <ClearQuizButton
-              queryOfQuizState={queryOfQuizState}
-              displayQuizState={displayQuizState}
-              setMessageStater={setMessageStater}
-              setDisplayQuizStater={setDisplayQuizStater}
+            <Button
+              label={'正解!!'}
+              attr={'button-array'}
+              variant="contained"
+              color="primary"
+              onClick={(e) =>
+                clearQuizAPI({
+                  queryOfQuizState,
+                  displayQuizState,
+                  setMessageStater,
+                  setDisplayQuizStater
+                })
+              }
             />
             <FailQuizButton
               queryOfQuizState={queryOfQuizState}
