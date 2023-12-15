60dcd288c3218f3a0dc571310535ccdf1d3c6157
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2023-12-15 21:19:07 +0900

    GetImageOfQuizButton削除・置き換え

--- frontend-next/src/components/ui-forms/quizzer/getQuiz/getQuizButtonGroup/GetQuizButtonGroup.tsx
+++ frontend-next/src/components/ui-forms/quizzer/getQuiz/getQuizButtonGroup/GetQuizButtonGroup.tsx
@@ -3,8 +3,9 @@ import { DisplayQuizState, MessageState, QueryOfQuizState } from '../../../../..
 import { GetRandomQuizButton } from '@/components/ui-parts/button-patterns/getRandomQuiz/GetRandomQuiz.button';
 import { GetWorstRateQuizButton } from '@/components/ui-parts/button-patterns/getWorstRateQuiz/GetWorstRateQuiz.button';
 import { GetMinimumClearQuizButton } from '@/components/ui-parts/button-patterns/getMinimumClearQuiz/GetMinimumClearQuiz.button';
-import { GetImageOfQuizButton } from '@/components/ui-parts/button-patterns/getImageOfQuiz/GetImageOfQuiz.button';
 import { GetQuizButton } from '@/components/ui-parts/button-patterns/getQuiz/GetQuiz.button';
+import { getImageOfQuizAPI } from '@/common/ButtonAPI';
+import { Button } from '@/components/ui-elements/button/Button';
 
 interface GetQuizButtonGroupProps {
   queryOfQuizState: QueryOfQuizState;
@@ -44,11 +45,15 @@ export const GetQuizButtonGroup = ({
         setMessageStater={setMessageStater}
         setQueryofQuizStater={setQueryofQuizStater}
       />
-      <GetImageOfQuizButton
-        queryOfQuizState={queryOfQuizState}
-        setDisplayQuizStater={setDisplayQuizStater}
-        setMessageStater={setMessageStater}
-        setQueryofQuizStater={setQueryofQuizStater}
+      <Button
+        label={'画像表示'}
+        attr={'button-array'}
+        variant="contained"
+        color="info"
+        disabled={true}
+        onClick={(e) =>
+          getImageOfQuizAPI({ queryOfQuizState, setMessageStater, setDisplayQuizStater, setQueryofQuizStater })
+        }
       />
     </>
   );
