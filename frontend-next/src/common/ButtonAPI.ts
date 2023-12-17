c6267cbf194dd94a1532a0e0ebedb9dbb4fec68d
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2023-12-17 13:17:34 +0900

    AddSayingButton  削除・置き換え

--- frontend-next/src/common/ButtonAPI.ts
+++ frontend-next/src/common/ButtonAPI.ts
@@ -1203,3 +1203,52 @@ export const addBookAPI = ({ bookName, setMessageStater, setBooklistoption }: Ad
   );
   getBook(setMessageStater, setBooklistoption);
 };
+
+interface AddSayingButtonProps {
+  selectedBookId: number;
+  inputSaying: string;
+  attr?: string;
+  setMessageStater?: React.Dispatch<React.SetStateAction<MessageState>>;
+  setBooklistoption?: React.Dispatch<React.SetStateAction<PullDownOptionState[]>>;
+}
+
+export const addSayingAPI = ({
+  selectedBookId,
+  inputSaying,
+  setMessageStater,
+  setBooklistoption
+}: AddSayingButtonProps) => {
+  // 設定ステートない場合はreturn(storybook表示用に設定)
+  if (!setMessageStater || !setBooklistoption) {
+    return;
+  }
+
+  if (!selectedBookId) {
+    setMessageStater({ message: 'エラー:本名を選択して下さい', messageColor: 'error', isDisplay: true });
+    return;
+  } else if (!inputSaying || inputSaying === '') {
+    setMessageStater({ message: 'エラー:格言を入力して下さい', messageColor: 'error', isDisplay: true });
+    return;
+  }
+
+  setMessageStater({ message: '通信中...', messageColor: '#d3d3d3', isDisplay: true });
+  post(
+    '/saying',
+    {
+      book_id: selectedBookId,
+      saying: inputSaying
+    },
+    (data: ProcessingApiReponse) => {
+      if (data.status === 200 || data.status === 201) {
+        setMessageStater({
+          message: `新規格言「${inputSaying}」を追加しました`,
+          messageColor: 'success.light',
+          isDisplay: true
+        });
+      } else {
+        setMessageStater({ message: 'エラー:外部APIとの連携に失敗しました', messageColor: 'error', isDisplay: true });
+      }
+    }
+  );
+  getBook(setMessageStater, setBooklistoption);
+};
