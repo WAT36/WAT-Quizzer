c19aaf2ec0b66aef686057bf0bc9aa26b1615372
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2023-12-24 15:17:02 +0900

    catch error retest

--- frontend-next/src/pages/englishBot/addWord.tsx
+++ frontend-next/src/pages/englishBot/addWord.tsx
@@ -449,7 +449,7 @@ export default function EnglishBotAddWordPage() {
         }
       }
     ).catch((err) => {
-      console.error(`API Error2. ${JSON.stringify(err)}`);
+      console.error(`API Error2. ${JSON.stringify(err)},err:${err}`);
       setMessage({
         message: 'エラー:外部APIとの連携に失敗しました',
         messageColor: 'error',
