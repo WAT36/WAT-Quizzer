60f1d9d3e19408522ffc162738d8fdfe6bfede96
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2024-01-21 23:01:40 +0900

    SearchResultTableコンポーネント 作成し適用

--- frontend-next/src/pages/quizzer/searchQuiz.tsx
+++ frontend-next/src/pages/quizzer/searchQuiz.tsx
@@ -14,6 +14,7 @@ import { Title } from '@/components/ui-elements/title/Title';
 import { SearchQueryForm } from '@/components/ui-forms/quizzer/searchQuiz/searchQueryForm/SearchQueryForm';
 import { searchQuizAPI } from '@/common/ButtonAPI';
 import { Button } from '@/components/ui-elements/button/Button';
+import { SearchResultTable } from '@/components/ui-elements/searchResultTable/SearchResultTable';
 
 export default function SearchQuizPage() {
   const [queryOfSearchQuizState, setQueryOfSearchQuizState] = useState<QueryOfSearchQuizState>({
@@ -301,16 +302,7 @@ export default function SearchQuizPage() {
           onClick={(e) => searchQuizAPI({ queryOfSearchQuizState, setMessage, setSearchResult })}
         />
 
-        <div style={searchedTableStyle}>
-          <DataGrid
-            rows={searchResult}
-            columns={columns}
-            pageSizeOptions={[15]}
-            checkboxSelection
-            disableRowSelectionOnClick
-            onRowSelectionModelChange={(selectionModel, details) => registerCheckedIdList(selectionModel)}
-          />
-        </div>
+        <SearchResultTable searchResult={searchResult} columns={columns} setCheckedIdList={setCheckedIdList} />
 
         <FormGroup style={groupStyle} row>
           チェックした問題全てにカテゴリ「
