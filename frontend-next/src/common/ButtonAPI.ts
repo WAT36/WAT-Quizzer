9263dbf682ca356214010d404b9a397229b5ea39
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2024-02-08 00:31:17 +0900

    単語詳細画面にサブ出典追加セクションを追加、そこから追加できるようにした

--- frontend-next/src/common/ButtonAPI.ts
+++ frontend-next/src/common/ButtonAPI.ts
@@ -25,7 +25,8 @@ import {
   QueryOfQuizState,
   QueryOfSearchQuizState,
   WordMeanData,
-  WordSourceData
+  WordSourceData,
+  WordSubSourceData
 } from '../../interfaces/state';
 import { del, get, patch, post, put } from './API';
 import { generateQuizSentense, getBook } from './response';
@@ -1754,6 +1755,83 @@ export const editEnglishWordSourceAPI = async ({
         if (setWordSourceData) {
           setWordSourceData(editedWordSourceData);
         }
+      } else {
+        if (setMessage) {
+          setMessage({
+            message: 'エラー:外部APIとの連携に失敗しました',
+            messageColor: 'error',
+            isDisplay: true
+          });
+        }
+      }
+    }
+  ).catch((err) => {
+    console.error(`API Error2. ${JSON.stringify(err)},${err}`);
+    if (setMessage) {
+      setMessage({
+        message: 'エラー:外部APIとの連携に失敗しました',
+        messageColor: 'error',
+        isDisplay: true
+      });
+    }
+  });
+};
+
+interface AddEnglishWordSubSourceButtonProps {
+  wordId: number;
+  subSourceName: string;
+  wordSubSourceData: WordSubSourceData[];
+  setMessage?: React.Dispatch<React.SetStateAction<MessageState>>;
+  setModalIsOpen?: React.Dispatch<React.SetStateAction<boolean>>;
+  setSubSourceName?: React.Dispatch<React.SetStateAction<string>>;
+  setWordSubSourceData?: React.Dispatch<React.SetStateAction<WordSubSourceData[]>>;
+}
+
+// TODO ここのAPI部分は分けたい
+export const addEnglishWordSubSourceAPI = async ({
+  wordId,
+  subSourceName,
+  wordSubSourceData,
+  setMessage,
+  setModalIsOpen,
+  setSubSourceName,
+  setWordSubSourceData
+}: AddEnglishWordSubSourceButtonProps) => {
+  if (setModalIsOpen) {
+    setModalIsOpen(false);
+  }
+  if (!subSourceName || subSourceName === '') {
+    if (setMessage) {
+      setMessage({ message: 'エラー:出典を選択して下さい', messageColor: 'error', isDisplay: true });
+    }
+    return;
+  }
+
+  await put(
+    '/english/word/subsource',
+    {
+      wordId: wordId,
+      subSource: subSourceName
+    },
+    (data: ProcessingApiReponse) => {
+      if (data.status === 200 || data.status === 201) {
+        if (setMessage) {
+          setMessage({
+            message: 'Success!! 編集に成功しました',
+            messageColor: 'success.light',
+            isDisplay: true
+          });
+        }
+
+        const editedWordSubSourceData = wordSubSourceData;
+        editedWordSubSourceData.push({
+          subSourceName: subSourceName
+        });
+        if (setWordSubSourceData) {
+          setWordSubSourceData(editedWordSubSourceData);
+        }
+
+        setSubSourceName && setSubSourceName('');
       } else {
         if (setMessage) {
           setMessage({
