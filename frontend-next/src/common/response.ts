73cb98c95268fe8bca3b243d0c139d87090259ac
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2024-02-15 00:06:40 +0900

    フロントから単語熟語統計データとるAPI作成

--- frontend-next/src/common/response.ts
+++ frontend-next/src/common/response.ts
@@ -1,5 +1,10 @@
 import { GetPopularEventResponse, GetSelfHelpBookResponse, ProcessingApiReponse } from '../../interfaces/api/response';
-import { PartofSpeechApiResponse, QuizFileApiResponse, QuizViewApiResponse } from '../../interfaces/db';
+import {
+  PartofSpeechApiResponse,
+  QuizFileApiResponse,
+  QuizViewApiResponse,
+  WordSummaryApiResponse
+} from '../../interfaces/db';
 import { MessageState, PullDownOptionState } from '../../interfaces/state';
 import { get } from './API';
 
@@ -185,6 +190,37 @@ export const getPartOfSpeechList = (
   } else {
     // 既にsession storageに値が入っている場合はそれを利用する
     setPartOfSpeechOption(JSON.parse(savedFileList));
+    setMessageStater({
+      message: '　',
+      messageColor: 'common.black'
+    });
+  }
+};
+
+// englishbot用 単語熟語統計データを取ってくる
+export const getWordSummaryData = (
+  setMessageStater: React.Dispatch<React.SetStateAction<MessageState>>,
+  setWordSummary: React.Dispatch<React.SetStateAction<WordSummaryApiResponse[]>>
+) => {
+  setMessageStater({ message: '通信中...', messageColor: '#d3d3d3' });
+
+  const storageKey = 'wordSummaryData';
+  const savedSummaryData = sessionStorage.getItem(storageKey);
+  if (!savedSummaryData) {
+    get('/english/word/summary', (data: ProcessingApiReponse) => {
+      if (data.status === 200) {
+        const result: WordSummaryApiResponse[] = data.body as WordSummaryApiResponse[];
+        setWordSummary(result);
+        setMessageStater({ message: '　', messageColor: 'common.black' });
+        // session storageに保存
+        sessionStorage.setItem(storageKey, JSON.stringify(result));
+      } else {
+        setMessageStater({ message: 'エラー:外部APIとの連携に失敗しました', messageColor: 'error' });
+      }
+    });
+  } else {
+    // 既にsession storageに値が入っている場合はそれを利用する
+    setWordSummary(JSON.parse(savedSummaryData));
     setMessageStater({
       message: '　',
       messageColor: 'common.black'
