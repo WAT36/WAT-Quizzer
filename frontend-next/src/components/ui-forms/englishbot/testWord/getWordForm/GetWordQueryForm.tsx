33bc0f6179f83addf42b7a9b9699c62128fe2d50
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2024-02-21 00:16:34 +0900

    （サブ出典用）登録日時の入力欄を画面に追加

--- frontend-next/src/components/ui-forms/englishbot/testWord/getWordForm/GetWordQueryForm.tsx
+++ frontend-next/src/components/ui-forms/englishbot/testWord/getWordForm/GetWordQueryForm.tsx
@@ -2,6 +2,10 @@ import React from 'react';
 import { FormControl, FormGroup } from '@mui/material';
 import { PullDownOptionState, QueryOfGetWordState } from '../../../../../../interfaces/state';
 import { PullDown } from '@/components/ui-elements/pullDown/PullDown';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import styles from './GetWordQueryForm.module.css';
 
 interface GetWordQueryFormProps {
   sourcelistoption: PullDownOptionState[];
@@ -29,6 +33,16 @@ export const GetWordQueryForm = ({
             }
           }}
         />
+      </FormControl>
+      <FormControl className={styles.row}>
+        登録日時指定：
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+          <DatePicker label="Start Date" className={styles.datePicker} />
+        </LocalizationProvider>
+        〜
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+          <DatePicker label="End Date" className={styles.datePicker} />
+        </LocalizationProvider>
       </FormControl>
     </FormGroup>
   );
