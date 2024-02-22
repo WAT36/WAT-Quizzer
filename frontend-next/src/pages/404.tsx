e15f70ca4523f10d59629b361af2014307cef4c6
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2024-02-22 23:29:05 +0900

    test dynamic routing対策

--- frontend-next/src/pages/404.tsx
+++ frontend-next/src/pages/404.tsx
@@ -1,13 +1,35 @@
 import { Title } from '@/components/ui-elements/title/Title';
 import Head from 'next/head';
+import Router from 'next/router';
+import { useEffect, useState } from 'react';
+
+// englishbot dynamic routing用
+const redirectPath = /^\/englishBot\/detailWord\/.+/g;
 
 export default function Custom404() {
-  return (
-    <>
-      <Head>
-        <Title label="404 Not Found"></Title>
-        <p>お探しのページは存在しません。</p>
-      </Head>
-    </>
-  );
+  const [isNotFound, setIsNotFound] = useState(false);
+
+  useEffect(() => {
+    // Your condition that can validate the URL
+    const pathName = window.location.pathname;
+
+    if (pathName.match(redirectPath)) {
+      Router.replace(pathName); // Redirect to the right page...
+    } else {
+      setIsNotFound(true);
+    }
+  }, []);
+
+  if (isNotFound) {
+    return (
+      <>
+        <Head>
+          <Title label="404 Not Found"></Title>
+          <p>お探しのページは存在しません。</p>
+        </Head>
+      </>
+    );
+  }
+
+  return null;
 }
