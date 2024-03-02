ceb0e51db2509d5afdaec67dc0fbba052ff187c6
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2024-03-02 10:02:11 +0900

    SearchRelatedWordSection story追加

--- frontend-next/src/components/ui-forms/englishbot/addExample/searchRelatedWordSection/SearchRelatedWordSection.stories.ts
+++ frontend-next/src/components/ui-forms/englishbot/addExample/searchRelatedWordSection/SearchRelatedWordSection.stories.ts
@@ -1,18 +1,18 @@
 import type { Meta, StoryObj } from '@storybook/react';
 
-import { AddExampleSection } from './AddExampleSection';
+import { SearchRelatedWordSection } from './SearchRelatedWordSection';
 
 // More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 const meta = {
-  title: 'Organisms/EnglishBot/AddExample/AddExampleSection',
-  component: AddExampleSection,
+  title: 'Organisms/EnglishBot/AddExample/SearchRelatedWordSection',
+  component: SearchRelatedWordSection,
   parameters: {
     // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/react/configure/story-layout
     layout: 'centered'
   },
   // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
   tags: ['autodocs']
-} satisfies Meta<typeof AddExampleSection>;
+} satisfies Meta<typeof SearchRelatedWordSection>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
@@ -20,6 +20,10 @@ type Story = StoryObj<typeof meta>;
 // More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
 export const Main: Story = {
   args: {
+    searchResult: [
+      { id: 1, wordmean_id: 1, partsofspeech: '名詞', meaning: '意味１' },
+      { id: 1, wordmean_id: 2, partsofspeech: '動詞', meaning: '意味２' }
+    ],
     inputExampleData: {}
   }
 };
