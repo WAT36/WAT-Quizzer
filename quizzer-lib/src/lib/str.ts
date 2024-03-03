c782f6ace6807885db2ada6ac9019b5aa3f2f16b
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2024-03-03 23:04:37 +0900

    共用パッケージquizzer-lib エクスポートできてなかったので対応

--- quizzer-lib/src/lib/str.ts
+++ quizzer-lib/src/lib/str.ts
@@ -1,17 +1,17 @@
 export const getRandomStr = () => {
-  let chars = 'abcdefghijklmnopqrstuvwxyz'
-  let rand_str = ''
+  let chars = 'abcdefghijklmnopqrstuvwxyz';
+  let rand_str = '';
   for (var i = 0; i < 8; i++) {
-    rand_str += chars.charAt(Math.floor(Math.random() * chars.length))
+    rand_str += chars.charAt(Math.floor(Math.random() * chars.length));
   }
-  return rand_str
-}
+  return rand_str;
+};
 
 export const getDateForSqlString = (date: Date) => {
-  const d = new Date(date)
-  const yyyy = d.getFullYear()
-  const mm = ('00' + (d.getMonth() + 1)).slice(-2)
-  const dd = ('00' + d.getDate()).slice(-2)
+  const d = new Date(date);
+  const yyyy = d.getFullYear();
+  const mm = ('00' + (d.getMonth() + 1)).slice(-2);
+  const dd = ('00' + d.getDate()).slice(-2);
 
-  return `${yyyy}-${mm}-${dd}`
-}
+  return `${yyyy}-${mm}-${dd}`;
+};
