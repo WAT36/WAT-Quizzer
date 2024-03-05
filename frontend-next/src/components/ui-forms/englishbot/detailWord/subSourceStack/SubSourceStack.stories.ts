ea7bfca8ae23be6cc5f4b4a00c80a47add46a506
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2024-03-05 18:27:19 +0900

    SubSourceStack story追加

---
+++ frontend-next/src/components/ui-forms/englishbot/detailWord/subSourceStack/SubSourceStack.stories.ts
@@ -0,0 +1,26 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { SubSourceStack } from './SubSourceStack';
+
+// More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+const meta = {
+  title: 'Organisms/EnglishBot/DetailWord/SubSourceStack',
+  component: SubSourceStack,
+  parameters: {
+    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/react/configure/story-layout
+    layout: 'centered'
+  },
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
+  tags: ['autodocs']
+} satisfies Meta<typeof SubSourceStack>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+// More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
+export const Main: Story = {
+  args: {
+    id: '0',
+    wordSubSourceData: [],
+    modalIsOpen: false
+  }
+};
