1815b80382691f9d196c9b12ac8b630338efc45c
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2024-03-14 19:46:49 +0900

    GetQuiz追加

---
+++ frontend-next/src/components/templates/pages/quizzer/GetQuiz.stories.tsx
@@ -0,0 +1,22 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import GetQuizPage from '../../../../pages/quizzer/getQuiz';
+import { RecoilRoot } from 'recoil';
+
+const meta = {
+  title: 'Templates/Quizzer/GetQuizPage',
+  component: GetQuizPage,
+  decorators: [(story) => <RecoilRoot>{story()} </RecoilRoot>],
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
+  tags: ['autodocs'],
+  parameters: {
+    // More on how to position stories at: https://storybook.js.org/docs/react/configure/story-layout
+    layout: 'fullscreen'
+  }
+} satisfies Meta<typeof GetQuizPage>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Main: Story = {
+  args: {}
+};
