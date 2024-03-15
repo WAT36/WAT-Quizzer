91178d3f9953b8850f9f1fb60e892993b0c5e724
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2024-03-15 21:42:10 +0900

    searchQuiz message recoil適用

--- frontend-next/src/pages/quizzer/searchQuiz.tsx
+++ frontend-next/src/pages/quizzer/searchQuiz.tsx
@@ -3,7 +3,7 @@ import { GridRowsProp } from '@mui/x-data-grid';
 import { columns } from '../../../utils/quizzer/SearchTable';
 import { Container } from '@mui/material';
 import { Layout } from '@/components/templates/layout/Layout';
-import { MessageState, PullDownOptionState, QueryOfSearchQuizState } from '../../../interfaces/state';
+import { PullDownOptionState, QueryOfSearchQuizState } from '../../../interfaces/state';
 import { getFileList } from '@/common/response';
 import { Title } from '@/components/ui-elements/title/Title';
 import { SearchQueryForm } from '@/components/ui-forms/quizzer/searchQuiz/searchQueryForm/SearchQueryForm';
@@ -11,6 +11,8 @@ import { searchQuizAPI } from '@/common/ButtonAPI';
 import { Button } from '@/components/ui-elements/button/Button';
 import { SearchResultTable } from '@/components/ui-elements/searchResultTable/SearchResultTable';
 import { EditSearchResultForm } from '@/components/ui-forms/quizzer/searchQuiz/editSearchResultForm/EditSearchResultForm';
+import { messageState } from '@/atoms/Message';
+import { useRecoilState } from 'recoil';
 
 export default function SearchQuizPage() {
   const [queryOfSearchQuizState, setQueryOfSearchQuizState] = useState<QueryOfSearchQuizState>({
@@ -18,11 +20,7 @@ export default function SearchQuizPage() {
     query: '',
     format: 'basic'
   });
-  const [message, setMessage] = useState<MessageState>({
-    message: '　',
-    messageColor: 'common.black',
-    isDisplay: false
-  });
+  const [message, setMessage] = useRecoilState(messageState);
   const [searchResult, setSearchResult] = useState<GridRowsProp>([] as GridRowsProp);
   const [filelistoption, setFilelistoption] = useState<PullDownOptionState[]>([]);
   const [categorylistoption, setCategorylistoption] = useState<PullDownOptionState[]>([]);
@@ -76,13 +74,7 @@ export default function SearchQuizPage() {
 
   return (
     <>
-      <Layout
-        mode="quizzer"
-        contents={contents()}
-        title={'問題検索'}
-        messageState={message}
-        setMessageStater={setMessage}
-      />
+      <Layout mode="quizzer" contents={contents()} title={'問題検索'} />
     </>
   );
 }
