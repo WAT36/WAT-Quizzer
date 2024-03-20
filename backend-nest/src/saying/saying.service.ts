6a5f3483e213232657bfb9842d823461c6f38090
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2024-03-20 16:43:31 +0900

    GetSayingAPIResponseDto追加適用

--- backend-nest/src/saying/saying.service.ts
+++ backend-nest/src/saying/saying.service.ts
@@ -5,6 +5,7 @@ import {
   AddBookAPIRequestDto,
   AddSayingAPIRequestDto,
   EditSayingAPIRequestDto,
+  GetSayingAPIResponseDto
 } from 'quizzer-lib';
 
 @Injectable()
@@ -12,11 +13,13 @@ export class SayingService {
   // 格言ランダム取得
   async getRandomSaying(book_id?: number) {
     try {
+      let result: GetSayingAPIResponseDto[]
       if (book_id) {
-        return await execQuery(SQL.SAYING.GET.RANDOM.BYBOOK, [book_id]);
+        result = await execQuery(SQL.SAYING.GET.RANDOM.BYBOOK, [book_id]);
       } else {
-        return await execQuery(SQL.SAYING.GET.RANDOM.ALL, []);
+        result = await execQuery(SQL.SAYING.GET.RANDOM.ALL, []);
       }
+      return result
     } catch (error: unknown) {
       if (error instanceof Error) {
         throw new HttpException(
---
+++ quizzer-lib/src/interfaces/api/saying/response.ts
@@ -0,0 +1,6 @@
+// 格言取得APIレスポンス型
+export interface GetSayingAPIResponseDto {
+  saying: string
+  explanation: string
+  name: string
+}
