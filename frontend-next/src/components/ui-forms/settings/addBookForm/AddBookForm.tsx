311992f086a5edced80fc51bc51d37ebff0c05d1
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2024-03-23 12:42:38 +0900

    AddBookForm調整

--- frontend-next/src/components/ui-forms/settings/addBookForm/AddBookForm.tsx
+++ frontend-next/src/components/ui-forms/settings/addBookForm/AddBookForm.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { MessageState, PullDownOptionState } from '../../../../../interfaces/state';
-import { CardContent, CardHeader } from '@mui/material';
+import { CardContent } from '@mui/material';
 import { Button } from '@/components/ui-elements/button/Button';
 import { addBookAPI } from '@/common/ButtonAPI';
 import { Card } from '@/components/ui-elements/card/Card';
 import { TextField } from '@/components/ui-elements/textField/TextField';
+import styles from '../Settings.module.css';
 
 interface AddBookFormProps {
   bookName: string;
@@ -16,13 +17,8 @@ interface AddBookFormProps {
 export const AddBookForm = ({ bookName, setBookName, setMessageStater, setBooklistoption }: AddBookFormProps) => {
   return (
     <>
-      <Card variant="outlined" attr="margin-vertical padding">
-        <CardHeader subheader="新規追加 - 啓発本" />
-        <CardContent
-          style={{
-            display: 'flex'
-          }}
-        >
+      <Card variant="outlined" subHeader="新規追加 - 啓発本" attr="margin-vertical padding">
+        <CardContent className={styles.cardContent}>
           <TextField label="新規啓発本名" className={['fullWidth']} variant="outlined" setStater={setBookName} />
           <Button
             label={'啓発本登録'}
