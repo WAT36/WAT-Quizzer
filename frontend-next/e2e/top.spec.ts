40d29f10b79ceec046472ef092e97e88d2d0d51d
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2024-03-24 15:57:20 +0900

    トップのE2E少し追加　ここまでで一旦取り込んでこれから適宜追加するようにしよう close #51

--- frontend-next/e2e/top.spec.ts
+++ frontend-next/e2e/top.spec.ts
@@ -1,8 +1,17 @@
 import { test, expect } from '@playwright/test';
 
-test('Quizzer Init', async ({ page }) => {
-  await page.goto(process.env.QUIZZER_FRONT_SERVER || '');
+test.describe('navigation', () => {
+  test.beforeEach(async ({ page }) => {
+    // Go to the starting url before each test.
+    await page.goto(process.env.QUIZZER_FRONT_SERVER || '');
+  });
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Quizzer/);
+  test('Quizzer Init', async ({ page }) => {
+    // Expect a title "to contain" a substring.
+    await expect(page).toHaveTitle(/Quizzer/);
+    // 格言で取得中。。から変わるか確認
+    await expect(page.locator('#saying')).not.toHaveText(/取得中.../);
+    // DBヘルスチェックで取得中。。から変わるか確認
+    await expect(page.locator('#db-health')).not.toHaveText(/取得中.../);
+  });
 });
