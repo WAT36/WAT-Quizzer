56cecaaf7dd0d15572938c77b1ee163337743fe6
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2024-03-28 19:21:36 +0900

    ボタン用問題チェック反転APIを分け

--- frontend-next/src/common/ButtonAPI.ts
+++ frontend-next/src/common/ButtonAPI.ts
@@ -1,7 +1,6 @@
 import { GridRowsProp } from '@mui/x-data-grid';
 import {
   DeleteQuizInfoState,
-  DisplayQuizState,
   DisplayWordTestState,
   EditQueryOfSaying,
   InputSayingState,
@@ -12,7 +11,6 @@ import {
   QueryOfGetAccuracyState,
   QueryOfGetWordState,
   QueryOfIntegrateToQuizState,
-  QueryOfQuizState,
   QueryOfSearchQuizState,
   QueryOfSearchWordState,
   WordMeanData,
@@ -24,7 +22,6 @@ import { getBook } from './response';
 import { InputExampleData } from '@/pages/englishBot/addExample';
 import {
   AddDataApiResponse,
-  CheckQuizApiResponse,
   EnglishBotTestFourChoiceResponse,
   GetAccuracyRateByCategoryAPIResponseDto,
   getDateForSqlString,
@@ -37,89 +34,6 @@ import {
   WordSearchAPIResponseDto
 } from 'quizzer-lib';
 import { meanOfAddWordDto, SendToAddWordApiData } from '../../interfaces/api/response';
-
-interface ReverseCheckQuizButtonProps {
-  queryOfQuizState: QueryOfQuizState;
-  displayQuizState: DisplayQuizState;
-  setMessageStater?: React.Dispatch<React.SetStateAction<MessageState>>;
-  setDisplayQuizStater?: React.Dispatch<React.SetStateAction<DisplayQuizState>>;
-}
-
-export const reverseCheckQuizAPI = async ({
-  queryOfQuizState,
-  displayQuizState,
-  setMessageStater,
-  setDisplayQuizStater
-}: ReverseCheckQuizButtonProps) => {
-  // 設定ステートない場合はreturn(storybook表示用に設定)
-  if (!setMessageStater || !setDisplayQuizStater) {
-    return;
-  }
-
-  if (queryOfQuizState.fileNum === -1) {
-    setMessageStater({
-      message: 'エラー:問題ファイルを選択して下さい',
-      messageColor: 'error',
-      isDisplay: true
-    });
-    return;
-  } else if (!queryOfQuizState.quizNum) {
-    setMessageStater({
-      message: 'エラー:問題番号を入力して下さい',
-      messageColor: 'error',
-      isDisplay: true
-    });
-    return;
-  } else if (!displayQuizState.quizSentense || !displayQuizState.quizAnswer) {
-    setMessageStater({
-      message: 'エラー:問題を出題してから登録して下さい',
-      messageColor: 'error',
-      isDisplay: true
-    });
-    return;
-  }
-
-  setMessageStater({
-    message: '通信中...',
-    messageColor: '#d3d3d3',
-    isDisplay: true
-  });
-  await post(
-    '/quiz/check',
-    {
-      format: queryOfQuizState.format,
-      file_num: queryOfQuizState.fileNum,
-      quiz_num: queryOfQuizState.quizNum
-    },
-    (data: ProcessingApiReponse) => {
-      if (data.status === 200 || data.status === 201) {
-        const res: CheckQuizApiResponse[] = data.body as CheckQuizApiResponse[];
-        setDisplayQuizStater({
-          ...displayQuizState,
-          checked: res[0].result
-        });
-        setMessageStater({
-          message: `問題[${queryOfQuizState.quizNum}] にチェック${res[0].result ? 'をつけ' : 'を外し'}ました`,
-          messageColor: 'success.light',
-          isDisplay: true
-        });
-      } else {
-        setMessageStater({
-          message: 'エラー:外部APIとの連携に失敗しました',
-          messageColor: 'error',
-          isDisplay: true
-        });
-      }
-    }
-  ).catch((err) => {
-    console.error(`API Error2. ${JSON.stringify(err)},${err}`);
-    setMessageStater({
-      message: 'エラー:外部APIとの連携に失敗しました',
-      messageColor: 'error',
-      isDisplay: true
-    });
-  });
-};
 
 interface SearchQuizButtonProps {
   queryOfSearchQuizState: QueryOfSearchQuizState;
