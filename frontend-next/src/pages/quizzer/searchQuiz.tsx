85081bad718ea26a00a081f507a007d9d4ec52a8
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2024-03-30 02:54:32 +0900

    ボタン用問題検索APIを分け

--- frontend-next/src/pages/quizzer/searchQuiz.tsx
+++ frontend-next/src/pages/quizzer/searchQuiz.tsx
@@ -7,12 +7,12 @@ import { PullDownOptionState, QueryOfSearchQuizState } from '../../../interfaces
 import { getFileList } from '@/common/response';
 import { Title } from '@/components/ui-elements/title/Title';
 import { SearchQueryForm } from '@/components/ui-forms/quizzer/searchQuiz/searchQueryForm/SearchQueryForm';
-import { searchQuizAPI } from '@/common/ButtonAPI';
 import { Button } from '@/components/ui-elements/button/Button';
 import { SearchResultTable } from '@/components/ui-elements/searchResultTable/SearchResultTable';
 import { EditSearchResultForm } from '@/components/ui-forms/quizzer/searchQuiz/editSearchResultForm/EditSearchResultForm';
 import { messageState } from '@/atoms/Message';
 import { useRecoilState } from 'recoil';
+import { searchQuizAPI } from '@/api/quiz/searchQuizAPI';
 
 type Props = {
   isMock?: boolean;
