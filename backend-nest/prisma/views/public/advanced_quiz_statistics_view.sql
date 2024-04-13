b2d4d38e31e6d97b47bc78d039a9a598164468e4
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2024-04-14 01:22:55 +0900

    統計用ビューに最終解答日時追加（LRU用）

--- backend-nest/prisma/views/public/advanced_quiz_statistics_view.sql
+++ backend-nest/prisma/views/public/advanced_quiz_statistics_view.sql
@@ -18,30 +18,58 @@ SELECT
         COALESCE(corrected_data.clear_count, (0) :: bigint) + COALESCE(incorrected_data.fail_count, (0) :: bigint)
       )
     )
-  END AS accuracy_rate
+  END AS accuracy_rate,
+  all_log_data.last_answer_log
 FROM
   (
     (
-      advanced_quiz
+      (
+        advanced_quiz
+        LEFT JOIN (
+          SELECT
+            answer_log.file_num,
+            answer_log.quiz_num,
+            count(*) AS clear_count
+          FROM
+            answer_log
+          WHERE
+            (
+              (answer_log.is_corrected = TRUE)
+              AND (answer_log.quiz_format_id <> 1)
+            )
+          GROUP BY
+            answer_log.file_num,
+            answer_log.quiz_num
+        ) corrected_data ON (
+          (
+            (advanced_quiz.file_num = corrected_data.file_num)
+            AND (advanced_quiz.quiz_num = corrected_data.quiz_num)
+          )
+        )
+      )
       LEFT JOIN (
         SELECT
           answer_log.file_num,
           answer_log.quiz_num,
-          count(*) AS clear_count
+          count(*) AS fail_count
         FROM
           answer_log
         WHERE
           (
-            (answer_log.is_corrected = TRUE)
+            (answer_log.is_corrected = false)
             AND (answer_log.quiz_format_id <> 1)
           )
         GROUP BY
           answer_log.file_num,
           answer_log.quiz_num
-      ) corrected_data ON (
+      ) incorrected_data ON (
         (
-          (advanced_quiz.file_num = corrected_data.file_num)
-          AND (advanced_quiz.quiz_num = corrected_data.quiz_num)
+          (
+            advanced_quiz.file_num = incorrected_data.file_num
+          )
+          AND (
+            advanced_quiz.quiz_num = incorrected_data.quiz_num
+          )
         )
       )
     )
@@ -49,25 +77,18 @@ FROM
       SELECT
         answer_log.file_num,
         answer_log.quiz_num,
-        count(*) AS fail_count
+        max(answer_log.created_at) AS last_answer_log
       FROM
         answer_log
       WHERE
-        (
-          (answer_log.is_corrected = false)
-          AND (answer_log.quiz_format_id <> 1)
-        )
+        (answer_log.quiz_format_id <> 1)
       GROUP BY
         answer_log.file_num,
         answer_log.quiz_num
-    ) incorrected_data ON (
+    ) all_log_data ON (
       (
-        (
-          advanced_quiz.file_num = incorrected_data.file_num
-        )
-        AND (
-          advanced_quiz.quiz_num = incorrected_data.quiz_num
-        )
+        (advanced_quiz.file_num = all_log_data.file_num)
+        AND (advanced_quiz.quiz_num = all_log_data.quiz_num)
       )
     )
   );
