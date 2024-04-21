1aecf8eac082767ea0a91421d5e4cd520640b133
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2024-04-21 22:26:05 +0900

    generateQuizSentense修正

--- quizzer-lib/src/lib/str.ts
+++ quizzer-lib/src/lib/str.ts
@@ -21,33 +21,35 @@ export const getDateForSqlString = (date: Date) => {
 // 問題取得系APIの返り値から問題文を生成する
 export const generateQuizSentense = (
   format: string,
-  res: GetQuizApiResponseDto[]
+  res: GetQuizApiResponseDto
 ) => {
   if (format === '4choice') {
     const choices = []
-    choices.push(res[0].answer)
-    for (let i = 0; i < res.length; i++) {
-      choices.push(res[i].dummy_choice_sentense || '')
+    choices.push(res.answer)
+    if (res.dummy_choice) {
+      for (let i = 0; i < res.dummy_choice.length; i++) {
+        choices.push(res.dummy_choice[i].dummy_choice_sentense || '')
+      }
     }
     // 選択肢の配列をランダムに並び替える
     const choiceName = ['A', 'B', 'C', 'D']
     choiceName.sort((a, b) => 0.5 - Math.random())
 
     return {
-      quizSentense: `[${res[0].file_num}-${res[0].quiz_num}]${
-        res[0].quiz_sentense
-      }${
-        res[0].accuracy_rate
-          ? '(正解率' + Number(res[0].accuracy_rate).toFixed(2) + '%)'
+      quizSentense: `[${res.file_num}-${res.quiz_num}]${res.quiz_sentense}${
+        res.advanced_quiz_statistics_view?.accuracy_rate
+          ? '(正解率' +
+            Number(res.advanced_quiz_statistics_view.accuracy_rate).toFixed(2) +
+            '%)'
           : ''
       }
         A: ${choices[choiceName.indexOf('A')]}
         B: ${choices[choiceName.indexOf('B')]}
         C: ${choices[choiceName.indexOf('C')]}
         D: ${choices[choiceName.indexOf('D')]}`,
-      quizAnswer: `${choiceName[0]}: ${res[0].answer}`,
-      explanation: res[0].explanation
-        ? res[0].explanation
+      quizAnswer: `${choiceName[0]}: ${res.answer}`,
+      explanation: res.advanced_quiz_explanation
+        ? res.advanced_quiz_explanation.explanation
             .replaceAll('{c}', choiceName[0])
             .replaceAll('{d1}', choiceName[1])
             .replaceAll('{d2}', choiceName[2])
@@ -56,15 +58,23 @@ export const generateQuizSentense = (
     }
   } else {
     return {
-      quizSentense: `[${res[0].file_num}-${res[0].quiz_num}]${
-        res[0].quiz_sentense
-      }${
-        res[0].accuracy_rate
-          ? '(正解率' + Number(res[0].accuracy_rate).toFixed(2) + '%)'
+      quizSentense: `[${res.file_num}-${res.quiz_num}]${res.quiz_sentense}${
+        res.quiz_statistics_view
+          ? '(正解率' +
+            Number(
+              res.quiz_statistics_view
+                ? res.quiz_statistics_view.accuracy_rate
+                : res.advanced_quiz_statistics_view
+                ? res.advanced_quiz_statistics_view.accuracy_rate
+                : NaN
+            ).toFixed(2) +
+            '%)'
           : ''
       }`,
-      quizAnswer: res[0].answer,
-      explanation: res[0].explanation
+      quizAnswer: res.answer,
+      explanation: res.advanced_quiz_explanation
+        ? res.advanced_quiz_explanation.explanation
+        : '(なし)'
     }
   }
 }
