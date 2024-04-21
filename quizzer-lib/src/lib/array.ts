fb13bf3c1bfb4a36b74527bfed3dc3e5ec026e62
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2024-04-22 00:24:35 +0900

    配列からランダムに要素とるlibb関数　少し修正＆追加

--- quizzer-lib/src/lib/array.ts
+++ quizzer-lib/src/lib/array.ts
@@ -1,10 +1,10 @@
 // リストからランダムにx個要素を取り出す
-export const getRandomElementsFromArray = (arr: any[], n: number) => {
+export const getRandomElementsFromArray = <T>(arr: T[], n: number) => {
   if (n >= arr.length) {
     return arr
   }
 
-  const extractArr = []
+  const extractArr: T[] = []
   const iSet = new Set()
   while (iSet.size < n) {
     let random = Math.floor(Math.random() * arr.length)
@@ -17,4 +17,13 @@ export const getRandomElementsFromArray = (arr: any[], n: number) => {
   }
 
   return extractArr
+}
+
+// リストからランダムに1個要素を取り出す
+export const getRandomElementFromArray = <T>(arr: T[]) => {
+  if (arr.length === 0) {
+    return
+  }
+  const random = Math.floor(Math.random() * arr.length)
+  return arr[random]
 }
