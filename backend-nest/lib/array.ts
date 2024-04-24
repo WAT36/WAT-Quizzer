2a7218e426d9a9643ead3063c9a8ea91dbf4ded2
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2024-04-24 19:25:21 +0900

    とりあえずprisma化完了やっと。 close #190

--- backend-nest/lib/array.ts
+++ backend-nest/lib/array.ts
@@ -1,7 +1,5 @@
 export function getDifferenceArray(arrayA: number[], arrayB: number[]) {
-    const setA = new Set(arrayA)
-    const setB = new Set(arrayB)
-    return Array.from(new Set(
-      [...setA].filter(element => !setB.has(element))
-    ));
+  const setA = new Set(arrayA);
+  const setB = new Set(arrayB);
+  return Array.from(new Set([...setA].filter((element) => !setB.has(element))));
 }
