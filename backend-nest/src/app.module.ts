a46bd89a0878c138b028032133532c535d4df8e4
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2024-04-24 20:58:28 +0900

    scrapingモジュールも削除

--- backend-nest/src/app.module.ts
+++ backend-nest/src/app.module.ts
@@ -5,16 +5,9 @@ import { QuizModule } from './quiz/quiz.module';
 import { CategoryModule } from './category/category.module';
 import { EnglishModule } from './english/english.module';
 import { SayingModule } from './saying/saying.module';
-import { ScrapeModule } from './scrape/scrape.module';
 
 @Module({
-  imports: [
-    QuizModule,
-    CategoryModule,
-    EnglishModule,
-    SayingModule,
-    ScrapeModule,
-  ],
+  imports: [QuizModule, CategoryModule, EnglishModule, SayingModule],
   controllers: [AppController],
   providers: [AppService],
 })
