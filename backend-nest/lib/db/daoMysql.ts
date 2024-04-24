2ee09b33bdadbbec0ff0a0ca42f92cae0c29bda4
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2024-04-24 20:58:06 +0900

    mysqlようもジュールを減らすためにここは一度コメントアウト

--- backend-nest/lib/db/daoMysql.ts
+++ backend-nest/lib/db/daoMysql.ts
@@ -1,115 +1,117 @@
-import 'dotenv/config';
-import { getDbUrl } from '../aws/secrets';
-import { createConnection } from 'mysql2';
-import { TransactionQuery } from '../../interfaces/db';
+// TODO node_modules節約のためにmysql2削除したので全てコメントアウト また使うときあったら復活させる
 
-// SQLを実行する
-export const execQuery = async (query: string, value: (string | number)[]) => {
-  // DB接続
-  const dbUrl = await getDbUrl();
-  const connection = createConnection(dbUrl);
+// import 'dotenv/config';
+// import { getDbUrl } from '../aws/secrets';
+// import { createConnection } from 'mysql2';
+// import { TransactionQuery } from '../../interfaces/db';
 
-  try {
-    // クエリ実行
-    const result: any = await new Promise((resolve, reject) => {
-      connection.query(query, value, (error, result) => {
-        if (error) {
-          reject(error);
-        }
-        resolve(result);
-      });
-    });
-    // 接続終了
-    await connection.end();
-    return result;
-  } catch (error) {
-    // 接続終了
-    await connection.end();
-    throw error;
-  }
-};
+// // SQLを実行する
+// export const execQuery = async (query: string, value: (string | number)[]) => {
+//   // DB接続
+//   const dbUrl = await getDbUrl();
+//   const connection = createConnection(dbUrl);
 
-// SQLトランザクションを実行する
-export const execTransaction = async (queries: TransactionQuery[]) => {
-  // DB接続
-  const dbUrl = await getDbUrl();
-  const connection = createConnection(dbUrl);
-  const result = [];
-  try {
-    // トランザクション開始
-    await beginTransaction(connection);
-    // クエリ実行
-    for (let i = 0; i < queries.length; i++) {
-      const { query, value } = queries[i];
-      const result_i = await execEachQueryInTransaction(
-        connection,
-        query,
-        value,
-      );
-      result.push(result_i);
-    }
-    // コミット
-    await commit(connection);
-    // 接続終了
-    await connection.end();
-    return result;
-  } catch (error) {
-    // ロールバック
-    await rollback(connection);
-    // 接続終了
-    await connection.end();
-    throw error;
-  }
-};
+//   try {
+//     // クエリ実行
+//     const result: any = await new Promise((resolve, reject) => {
+//       connection.query(query, value, (error, result) => {
+//         if (error) {
+//           reject(error);
+//         }
+//         resolve(result);
+//       });
+//     });
+//     // 接続終了
+//     await connection.end();
+//     return result;
+//   } catch (error) {
+//     // 接続終了
+//     await connection.end();
+//     throw error;
+//   }
+// };
 
-// トランザクション開始
-const beginTransaction = (connection) => {
-  return new Promise<void>((resolve, reject) => {
-    connection.beginTransaction((err) => {
-      if (err) {
-        reject(err);
-      } else {
-        resolve();
-      }
-    });
-  });
-};
+// // SQLトランザクションを実行する
+// export const execTransaction = async (queries: TransactionQuery[]) => {
+//   // DB接続
+//   const dbUrl = await getDbUrl();
+//   const connection = createConnection(dbUrl);
+//   const result = [];
+//   try {
+//     // トランザクション開始
+//     await beginTransaction(connection);
+//     // クエリ実行
+//     for (let i = 0; i < queries.length; i++) {
+//       const { query, value } = queries[i];
+//       const result_i = await execEachQueryInTransaction(
+//         connection,
+//         query,
+//         value,
+//       );
+//       result.push(result_i);
+//     }
+//     // コミット
+//     await commit(connection);
+//     // 接続終了
+//     await connection.end();
+//     return result;
+//   } catch (error) {
+//     // ロールバック
+//     await rollback(connection);
+//     // 接続終了
+//     await connection.end();
+//     throw error;
+//   }
+// };
 
-// 単クエリ実行
-const execEachQueryInTransaction = (connection, statement, params) => {
-  return new Promise((resolve, reject) => {
-    connection.query(statement, params, (err, results, fields) => {
-      if (err) {
-        reject(err);
-      } else {
-        resolve(results);
-      }
-    });
-  });
-};
+// // トランザクション開始
+// const beginTransaction = (connection) => {
+//   return new Promise<void>((resolve, reject) => {
+//     connection.beginTransaction((err) => {
+//       if (err) {
+//         reject(err);
+//       } else {
+//         resolve();
+//       }
+//     });
+//   });
+// };
 
-// コミット
-const commit = (connection) => {
-  return new Promise((resolve, reject) => {
-    connection.commit((err) => {
-      if (err) {
-        reject(err);
-      } else {
-        resolve(err);
-      }
-    });
-  });
-};
+// // 単クエリ実行
+// const execEachQueryInTransaction = (connection, statement, params) => {
+//   return new Promise((resolve, reject) => {
+//     connection.query(statement, params, (err, results, fields) => {
+//       if (err) {
+//         reject(err);
+//       } else {
+//         resolve(results);
+//       }
+//     });
+//   });
+// };
 
-// ロールバック
-const rollback = (connection) => {
-  return new Promise<void>((resolve, reject) => {
-    connection.rollback((err) => {
-      if (err) {
-        reject(err);
-      } else {
-        resolve();
-      }
-    });
-  });
-};
+// // コミット
+// const commit = (connection) => {
+//   return new Promise((resolve, reject) => {
+//     connection.commit((err) => {
+//       if (err) {
+//         reject(err);
+//       } else {
+//         resolve(err);
+//       }
+//     });
+//   });
+// };
+
+// // ロールバック
+// const rollback = (connection) => {
+//   return new Promise<void>((resolve, reject) => {
+//     connection.rollback((err) => {
+//       if (err) {
+//         reject(err);
+//       } else {
+//         resolve();
+//       }
+//     });
+//   });
+// };
