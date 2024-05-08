a767910471830153ca26c072be25c5e886607c2a
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2024-05-08 20:11:02 +0900

    API Authorizationヘッダーを追加　x-api-keyは削除

--- frontend-next/src/api/API.ts
+++ frontend-next/src/api/API.ts
@@ -1,20 +1,24 @@
 import { ProcessingApiReponse } from 'quizzer-lib';
-import { getApiKey } from '../../lib/aws/secrets';
 
 export const baseURL: string = process.env.NEXT_PUBLIC_API_SERVER || '';
 
+// TODO メソッドごとに分けてるけどまとめられないか？
+// TODO getAPIKeyは削除する
 export const get = async (
   path: string,
   func: (data: ProcessingApiReponse) => any,
-  queryParam?: { [key: string]: string }
+  queryParam?: { [key: string]: string },
+  bodyData?: object
 ) => {
-  const key = await getApiKey();
   const query = queryParam ? `?${new URLSearchParams(queryParam)}` : '';
 
   const result = await fetch(baseURL + path + query, {
     method: 'GET',
+    body: bodyData ? JSON.stringify(bodyData) : null,
     headers: {
-      'x-api-key': key
+      ...(process.env.NEXT_PUBLIC_APP_ENV === 'local' && {
+        Authorization: 'Bearer ' + localStorage.getItem('apiAccessToken')
+      })
     }
   })
     .then((response) =>
@@ -31,13 +35,14 @@ export const get = async (
 };
 
 export const getApiAndGetValue = async (path: string, queryParam?: { [key: string]: string }) => {
-  const key = await getApiKey();
   const query = queryParam ? `?${new URLSearchParams(queryParam)}` : '';
 
   return await fetch(baseURL + path + query, {
     method: 'GET',
     headers: {
-      'x-api-key': key
+      ...(process.env.NEXT_PUBLIC_APP_ENV === 'local' && {
+        Authorization: 'Bearer ' + localStorage.getItem('apiAccessToken')
+      })
     }
   })
     .catch((error) => {
@@ -48,13 +53,14 @@ export const getApiAndGetValue = async (path: string, queryParam?: { [key: strin
 };
 
 export const post = async (path: string, jsondata: object, func: (data: ProcessingApiReponse) => void) => {
-  const key = await getApiKey();
   await fetch(baseURL + path, {
     method: 'POST',
     body: JSON.stringify(jsondata),
     headers: {
       'Content-Type': 'application/json',
-      'x-api-key': key
+      ...(process.env.NEXT_PUBLIC_APP_ENV === 'local' && {
+        Authorization: 'Bearer ' + localStorage.getItem('apiAccessToken')
+      })
     }
   })
     .catch((error) => {
@@ -71,13 +77,14 @@ export const post = async (path: string, jsondata: object, func: (data: Processi
 };
 
 export const put = async (path: string, jsondata: object, func: (data: ProcessingApiReponse) => void) => {
-  const key = await getApiKey();
   await fetch(baseURL + path, {
     method: 'PUT',
     body: JSON.stringify(jsondata),
     headers: {
       'Content-Type': 'application/json',
-      'x-api-key': key
+      ...(process.env.NEXT_PUBLIC_APP_ENV === 'local' && {
+        Authorization: 'Bearer ' + localStorage.getItem('apiAccessToken')
+      })
     }
   })
     .then((response) =>
@@ -93,13 +100,14 @@ export const put = async (path: string, jsondata: object, func: (data: Processin
 };
 
 export const del = async (path: string, jsondata: object, func: (data: ProcessingApiReponse) => void) => {
-  const key = await getApiKey();
   await fetch(baseURL + path, {
     method: 'DELETE',
     body: JSON.stringify(jsondata),
     headers: {
       'Content-Type': 'application/json',
-      'x-api-key': key
+      ...(process.env.NEXT_PUBLIC_APP_ENV === 'local' && {
+        Authorization: 'Bearer ' + localStorage.getItem('apiAccessToken')
+      })
     }
   })
     .then((response) =>
@@ -115,13 +123,14 @@ export const del = async (path: string, jsondata: object, func: (data: Processin
 };
 
 export const patch = async (path: string, jsondata: object, func: (data: ProcessingApiReponse) => void) => {
-  const key = await getApiKey();
   await fetch(baseURL + path, {
     method: 'PATCH',
     body: JSON.stringify(jsondata),
     headers: {
       'Content-Type': 'application/json',
-      'x-api-key': key
+      ...(process.env.NEXT_PUBLIC_APP_ENV === 'local' && {
+        Authorization: 'Bearer ' + localStorage.getItem('apiAccessToken')
+      })
     }
   })
     .then((response) =>
