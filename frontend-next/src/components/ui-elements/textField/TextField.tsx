454ca33ec3ab7c846d4787965a6f2d52fba708bf
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2024-05-08 13:46:29 +0900

    TextFieldにtype追加

--- frontend-next/src/components/ui-elements/textField/TextField.tsx
+++ frontend-next/src/components/ui-elements/textField/TextField.tsx
@@ -6,14 +6,16 @@ interface TextFieldProps {
   label: string;
   variant?: 'standard' | 'filled' | 'outlined';
   className?: string[];
+  type?: string;
   setStater?: React.Dispatch<React.SetStateAction<string>> | ((value: string) => void);
 }
 
-export const TextField = ({ label, variant, className, setStater }: TextFieldProps) => (
+export const TextField = ({ label, variant, className, type, setStater }: TextFieldProps) => (
   <MuiTextField
     className={[styles.textField].concat(className ? className.map((x) => styles[x] || '') : []).join(' ')}
     variant={variant || 'outlined'}
     label={label}
+    type={type || 'text'}
     onChange={(e) => {
       if (setStater) {
         setStater(e.target.value);
