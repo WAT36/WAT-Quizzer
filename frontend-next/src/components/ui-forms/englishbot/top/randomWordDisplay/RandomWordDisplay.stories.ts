d58c7f255434d172f1543802745a0d5622d19be5
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2024-05-13 00:55:17 +0900

    トップ用　ランダム英単語表示コンポーネント追加

---
+++ frontend-next/src/components/ui-forms/englishbot/top/randomWordDisplay/RandomWordDisplay.stories.ts
@@ -0,0 +1,66 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { RandomWordDisplay } from './RandomWordDisplay';
+
+// More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+const meta = {
+  title: 'Organisms/EnglishBot/Top/RandomWordDisplay',
+  component: RandomWordDisplay,
+  parameters: {
+    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/react/configure/story-layout
+    layout: 'centered'
+  },
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
+  tags: ['autodocs']
+} satisfies Meta<typeof RandomWordDisplay>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+// More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
+export const Main: Story = {
+  args: {
+    wordData: {
+      id: 1,
+      name: 'test',
+      pronounce: '',
+      mean: [
+        {
+          meaning: '意味１',
+          partsofspeech: {
+            name: '品詞１'
+          },
+          mean_source: [
+            {
+              source: {
+                name: '出典1-1'
+              }
+            },
+            {
+              source: {
+                name: '出典1-2'
+              }
+            }
+          ]
+        },
+        {
+          meaning: '意味２',
+          partsofspeech: {
+            name: '品詞２'
+          },
+          mean_source: [
+            {
+              source: {
+                name: '出典2-1'
+              }
+            },
+            {
+              source: {
+                name: '出典2-2'
+              }
+            }
+          ]
+        }
+      ]
+    }
+  }
+};
---
+++ frontend-next/src/components/ui-forms/englishbot/top/randomWordDisplay/RandomWordDisplay.tsx
@@ -0,0 +1,30 @@
+import { Card } from '@/components/ui-elements/card/Card';
+import { CardContent, Typography } from '@mui/material';
+import { GetRandomWordAPIResponse } from 'quizzer-lib';
+
+interface RandomWordDisplayProps {
+  wordData: GetRandomWordAPIResponse;
+}
+
+export const RandomWordDisplay = ({ wordData }: RandomWordDisplayProps) => {
+  return (
+    <Card variant="outlined" attr="rect-400 margin-vertical">
+      <CardContent>
+        <Typography variant="h6" component="h6" color="grey.700">
+          ランダムに１語
+        </Typography>
+        <Typography id="wordName" variant="h2" component="p" color="black">
+          {wordData.name}
+        </Typography>
+        {wordData.mean.map((data, index) => {
+          return (
+            <Typography key={index} variant="subtitle1" component="p" color="grey.800">
+              [{data.partsofspeech.name}] {data.meaning}
+            </Typography>
+          );
+        })}
+        ;
+      </CardContent>
+    </Card>
+  );
+};
