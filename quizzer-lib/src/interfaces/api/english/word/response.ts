307bfb45b90b1fc44f543f107f1be86e45196de8
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2024-05-13 00:54:21 +0900

    APIレスポンス型追加

--- quizzer-lib/src/interfaces/api/english/word/response.ts
+++ quizzer-lib/src/interfaces/api/english/word/response.ts
@@ -86,3 +86,21 @@ export interface GetSourceOfWordAPIResponseDto extends ApiResponse {
 export interface GetSubSourceOfWordAPIResponseDto extends ApiResponse {
   subsource: string
 }
+
+// 英単語ランダム取得APIレスポンス型
+export interface GetRandomWordAPIResponse extends ApiResponse {
+  id: number
+  name: string
+  pronounce: string
+  mean: {
+    meaning: string
+    partsofspeech: {
+      name: string
+    }
+    mean_source: {
+      source: {
+        name: string
+      }
+    }[]
+  }[]
+}
