73454c7db77489ee0097aed349f95bf8272270cb
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2024-05-14 00:29:55 +0900

    Cardのスタイル追加

--- frontend-next/src/components/ui-elements/card/Card.tsx
+++ frontend-next/src/components/ui-elements/card/Card.tsx
@@ -3,13 +3,15 @@ import { Card as MuiCard, CardHeader as MuiCardHeader } from '@mui/material';
 import styles from './Card.module.css';
 
 interface CardProps {
-  variant: string;
+  variant?: string;
   attr?: string;
   header?: string;
   subHeader?: string;
   children?: ReactNode;
 }
 
+// TODO このvariant使われていない？
+// TODO attr みたいなクラスの指定方法。他のコンポーネントもだけどなんかわかりにくい。string配列形式の方が良さそう
 export const Card = ({ variant = 'outlined', ...props }: CardProps) => {
   return (
     <>
