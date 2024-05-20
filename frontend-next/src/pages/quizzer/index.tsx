c20c3a0c0f1a6740ad180ae866686fe6e55b4935
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2024-05-21 00:01:48 +0900

    quizzerトップに問題ファイル統計グラフカード追加　ちょっと最高の余地あり、、グラフ何表示するかはもう少し考えるかも　close #206

--- frontend-next/src/pages/quizzer/index.tsx
+++ frontend-next/src/pages/quizzer/index.tsx
@@ -1,10 +1,36 @@
+import { getQuizFileStatisticsDataAPI } from '@/api/quiz/getQuizFileStatisticsDataAPI';
+import { messageState } from '@/atoms/Message';
 import { Layout } from '@/components/templates/layout/Layout';
-import React from 'react';
+import { FileStatisticsCard } from '@/components/ui-forms/quizzer/top/fileStatisticsCard/FileStatisticsCard';
+import { Container } from '@mui/material';
+import { QuizFileStatisticsApiResponse } from 'quizzer-lib';
+import React, { useEffect, useState } from 'react';
+import { useSetRecoilState } from 'recoil';
 
-export default function QuizzerTopPage() {
+type Props = {
+  isMock?: boolean;
+};
+
+export default function QuizzerTopPage({ isMock }: Props) {
+  const [quizFileStatisticsData, setQuizFileStatisticsData] = useState<QuizFileStatisticsApiResponse[]>([]);
+  const setMessage = useSetRecoilState(messageState);
+
+  // 問題ファイルリスト取得
+  useEffect(() => {
+    !isMock && Promise.all([getQuizFileStatisticsDataAPI({ setMessage, setQuizFileStatisticsData })]);
+  }, [isMock, setMessage]);
+
+  const contents = () => {
+    return (
+      <Container>
+        <FileStatisticsCard quizFileStatisticsData={quizFileStatisticsData} />
+      </Container>
+    );
+  };
+
   return (
     <>
-      <Layout mode="quizzer" contents={<p>準備中・・・\nここはダッシュボードか何かにする</p>} title={'Top'} />
+      <Layout mode="quizzer" contents={contents()} title={'Top'} />
     </>
   );
 }
