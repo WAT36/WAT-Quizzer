ac20a4957d6fa5d126e2cc0ccc513e2ecd63ca65
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2024-05-21 20:35:35 +0900

    prisma用　指定日内の条件json作る関数追加

--- quizzer-lib/src/lib/date.ts
+++ quizzer-lib/src/lib/date.ts
@@ -22,3 +22,48 @@ export const getPrismaYesterdayRange = () => {
     lt: today
   }
 }
+
+// prisma用　今日から数えてn日前内のデータを取りたい時の条件
+export const getPrismaPastDayRange = (n: number) => {
+  const nowDate = new Date()
+  const pastDay = new Date(
+    nowDate.getFullYear(),
+    nowDate.getMonth(),
+    nowDate.getDate() - n,
+    0,
+    0,
+    0
+  )
+  const pastDayTommorow = new Date(
+    nowDate.getFullYear(),
+    nowDate.getMonth(),
+    nowDate.getDate() - n + 1,
+    0,
+    0,
+    0
+  )
+  return {
+    gte: pastDay,
+    lt: pastDayTommorow
+  }
+}
+
+// Date型 n日前の日付形式を(文字列形式で)取得
+export const getPastDate = (n: number) => {
+  const nowDate = new Date()
+  const pastDay = new Date(
+    nowDate.getFullYear(),
+    nowDate.getMonth(),
+    nowDate.getDate() - n,
+    0,
+    0,
+    0
+  )
+  return pastDay
+    .toLocaleDateString('ja-JP', {
+      year: 'numeric',
+      month: '2-digit',
+      day: '2-digit'
+    })
+    .replaceAll('/', '-')
+}
