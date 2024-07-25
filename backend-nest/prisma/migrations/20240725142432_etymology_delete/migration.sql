88bd9f4b6bf98280b5f6463d5e5fcca9741c2037
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2024-07-25 23:25:09 +0900

    一旦語源テーブルを削除

---
+++ backend-nest/prisma/migrations/20240725142432_etymology_delete/migration.sql
@@ -0,0 +1,12 @@
+/*
+  Warnings:
+
+  - You are about to drop the `word_etymology` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `word_etymology_group` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropTable
+DROP TABLE "word_etymology";
+
+-- DropTable
+DROP TABLE "word_etymology_group";
