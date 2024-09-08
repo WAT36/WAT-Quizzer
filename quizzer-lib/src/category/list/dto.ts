e4ab64925240fe933ca7960d47053bc260475738
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2024-09-08 23:49:58 +0900

    カテゴリリストDTOだけとりあえず

---
+++ quizzer-lib/src/category/list/dto.ts
@@ -0,0 +1,14 @@
+import { ApiResponse } from '../../..'
+
+export interface GetCategoryListAPIRequestDto {
+  file_num: string
+}
+
+// カテゴリ取得APIレスポンス型
+export interface GetCategoryAPIResponseDto extends ApiResponse {
+  file_num?: number
+  category: string
+  created_at?: string
+  updated_at?: string
+  deleted_at?: string
+}
