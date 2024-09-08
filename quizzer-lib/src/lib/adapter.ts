3b4b0cf915ae1950b924123fad098a88eee41815
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2024-09-09 00:18:07 +0900

    カテゴリリストレスポンスからプルダウンへのアダプターをlibに追加

--- quizzer-lib/src/lib/adapter.ts
+++ quizzer-lib/src/lib/adapter.ts
@@ -1,4 +1,8 @@
-import { PullDownOptionDto, GetQuizFileApiResponseDto } from '../..'
+import {
+  PullDownOptionDto,
+  GetQuizFileApiResponseDto,
+  GetCategoryAPIResponseDto
+} from '../..'
 
 // 問題ファイルリスト取得API -> フロント用 問題ファイルプルダウン用DTO に変える
 export const quizFileListAPIResponseToPullDownAdapter = (
@@ -9,6 +13,20 @@ export const quizFileListAPIResponseToPullDownAdapter = (
     pulldown.push({
       value: String(x.file_num),
       label: x.file_nickname
+    })
+  })
+  return pulldown
+}
+
+// 問題カテゴリリスト取得API -> フロント用 カテゴリプルダウン用DTO に変える
+export const getCategoryListAPIResponseToPullDownAdapter = (
+  arr: GetCategoryAPIResponseDto[]
+) => {
+  const pulldown: PullDownOptionDto[] = []
+  arr.map((x) => {
+    pulldown.push({
+      value: x.category,
+      label: x.category
     })
   })
   return pulldown
