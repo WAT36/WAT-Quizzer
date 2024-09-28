64c6013c839c60a6a2dd649e0f0765ed7680cdfc
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2024-09-28 18:14:35 +0900

    問題取得APIレスポンス -> 問題編集APIリクエストのアダプター追加

--- quizzer-lib/src/lib/adapter.ts
+++ quizzer-lib/src/lib/adapter.ts
@@ -1,7 +1,9 @@
+import { EditQuizAPIRequestDto } from '../quiz/edit/dto'
 import {
   PullDownOptionDto,
   GetQuizFileApiResponseDto,
-  GetCategoryAPIResponseDto
+  GetCategoryAPIResponseDto,
+  GetQuizApiResponseDto
 } from '../..'
 
 // 問題ファイルリスト取得API -> フロント用 問題ファイルプルダウン用DTO に変える
@@ -30,4 +32,49 @@ export const getCategoryListAPIResponseToPullDownAdapter = (
     })
   })
   return pulldown
+}
+
+// 問題取得APIレスポンス -> 問題編集APIリクエスト
+export const getQuizAPIResponseToEditQuizAPIRequestAdapter = (
+  response: GetQuizApiResponseDto
+) => {
+  const result: EditQuizAPIRequestDto = {
+    // TODO 基礎応用統合したらいらなくなるので仮で-1
+    value: -1,
+
+    file_num: response.file_num,
+    quiz_num: response.quiz_num,
+    format: response.format,
+    question: response.quiz_sentense,
+    answer: response.answer,
+    category: response.quiz_category
+      ? response.quiz_category
+          .map((c) => {
+            return c.category
+          })
+          .join(',')
+      : '',
+    img_file: response.img_file,
+    matched_basic_quiz_id: response.quiz_basis_advanced_linkage
+      ? response.quiz_basis_advanced_linkage
+          .map((id) => {
+            return String(id.basis_quiz_id)
+          })
+          .join(',')
+      : '',
+    dummy1:
+      response.dummy_choice && response.dummy_choice.length > 0
+        ? response.dummy_choice[0].dummy_choice_sentense
+        : '', //四択問題のダミー選択肢１
+    dummy2:
+      response.dummy_choice && response.dummy_choice.length > 1
+        ? response.dummy_choice[1].dummy_choice_sentense
+        : '', //四択問題のダミー選択肢２
+    dummy3:
+      response.dummy_choice && response.dummy_choice.length > 2
+        ? response.dummy_choice[2].dummy_choice_sentense
+        : '', //四択問題のダミー選択肢３
+    explanation: response.advanced_quiz_explanation?.explanation // 解説
+  }
+  return result
 }
