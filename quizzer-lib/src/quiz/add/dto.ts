d9cca7a40a0f4d8027a4d3dde1f48c046efffbca
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2024-09-28 18:12:54 +0900

    問題追加API型修正

--- quizzer-lib/src/quiz/add/dto.ts
+++ quizzer-lib/src/quiz/add/dto.ts
@@ -1,24 +1,7 @@
 import { ApiResponse } from '../../..'
+import { EditQuizAPIRequestDto } from '../edit/dto'
 
-export interface AddQuizAPIRequestDto {
-  // TODO 基礎応用問題統合したら　このvalueは削除する？
-  value: number
-
-  //queryOfAddQuizState: QueryOfPutQuizState;
-  file_num: number
-  quiz_num: number
-  format?: string
-  formatValue?: number
-  question?: string
-  answer?: string
-  quiz_category?: string
-  img_file?: string
-  matched_basic_quiz_id?: string
-  dummy1?: string //四択問題のダミー選択肢１
-  dummy2?: string //四択問題のダミー選択肢２
-  dummy3?: string //四択問題のダミー選択肢３
-  explanation?: string // 解説
-}
+export type AddQuizAPIRequestDto = Omit<EditQuizAPIRequestDto, 'quiz_num'>
 
 // 問題追加APIのレスポンス（ログだけ）
 export interface AddQuizApiResponseDto extends ApiResponse {
--- quizzer-lib/src/quiz/add/mock.ts
+++ quizzer-lib/src/quiz/add/mock.ts
@@ -1,5 +1,4 @@
 export const initAddQuizRequestData = {
   file_num: -1,
-  quiz_num: -1,
   value: -1
 }
